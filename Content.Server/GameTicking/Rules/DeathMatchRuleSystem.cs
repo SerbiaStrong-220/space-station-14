1e50475b0d7238c5e5ab3e428c4961c0e73cf234
Asketss <askets999@gmail.com>
2023-06-04 17:58:20 +0200

    WIP доработано добавление аплинка игрокам в режиме ДМ - работает в похожий способ как при выполнении команды adduplick

--- Content.Server/GameTicking/Rules/DeathMatchRuleSystem.cs
+++ Content.Server/GameTicking/Rules/DeathMatchRuleSystem.cs
@@ -12,11 +12,10 @@ using Content.Server.PDA.Ringer;
 using Content.Server.Players;
 using Content.Server.Traitor;
 using Content.Shared.Roles;
-using Robust.Shared.Audio;
-using Robust.Shared.Log;
 using Robust.Shared.Utility;
 using System.Linq;
 using Robust.Shared.Prototypes;
+using Content.Shared.FixedPoint;
 
 namespace Content.Server.GameTicking.Rules;
 
@@ -92,9 +91,9 @@ public sealed class DeathMatchRuleSystem : GameRuleSystem<DeathMatchRuleComponen
     {
         base.ActiveTick(uid, component, gameRule, frameTime);
 
-        foreach (var traitor in _playerManager.ServerSessions)
+        foreach (var playerSession in _playerManager.ServerSessions)
         {
-            MakeTraitor(traitor);
+            AddUplink(playerSession);
         }
 
         // If the restart timer is active, that means the round is ending soon, no need to check for winners.
@@ -147,55 +146,18 @@ public sealed class DeathMatchRuleSystem : GameRuleSystem<DeathMatchRuleComponen
         component.RestartTimer = component.RestartDelay;
     }
 
-    public bool MakeTraitor(IPlayerSession traitor)
+    public void AddUplink(IPlayerSession session)
     {
-        var traitorRule = EntityQuery<TraitorRuleComponent>().FirstOrDefault();
-        if (traitorRule == null)
-        {
-            //todo fuck me this shit is awful
-            //no i wont fuck you, erp is against rules
-            GameTicker.StartGameRule("Traitor", out var ruleEntity);
-            traitorRule = Comp<TraitorRuleComponent>(ruleEntity);
-        }
-
-        var mind = traitor.Data.ContentData()?.Mind;
-        if (mind == null)
-        {
-            _sawmill.Info("Failed getting mind for picked traitor.");
-            return false;
-        }
-
-        if (mind.OwnedEntity is not { } entity)
-        {
-            Logger.ErrorS("preset", "Mind picked for traitor did not have an attached entity.");
-            return false;
-        }
-
-        // creadth: we need to create uplink for the antag.
-        // PDA should be in place already
-        DebugTools.AssertNotNull(mind.OwnedEntity);
-
-        var startingBalance = _cfg.GetCVar(CCVars.TraitorStartingBalance);
-
-        var pda = _uplink.FindUplinkTarget(mind.OwnedEntity!.Value);
-        if (pda == null || !_uplink.AddUplink(mind.OwnedEntity.Value, startingBalance))
-            return false;
-
-        // add the ringtone uplink and get its code for greeting
-        var code = AddComp<RingerUplinkComponent>(pda.Value).Code;
-
-        var antagPrototype = _prototypeManager.Index<AntagPrototype>(traitorRule.TraitorPrototypeId);
-        var traitorRole = new TraitorRole(mind, antagPrototype);
-        mind.AddRole(traitorRole);
-        traitorRole.GreetTraitor(traitorRule.Codewords, code);
-
-        var maxDifficulty = _cfg.GetCVar(CCVars.TraitorMaxDifficulty);
-        var maxPicks = _cfg.GetCVar(CCVars.TraitorMaxPicks);
-
-        //give traitors their codewords and uplink code to keep in their character info menu
-        traitorRole.Mind.Briefing = Loc.GetString("traitor-role-codewords-short", ("codewords", string.Join(", ", traitorRule.Codewords)))
-            + "\n" + Loc.GetString("traitor-role-uplink-code-short", ("code", string.Join("", code)));
-
-        return true;
+        if (session?.AttachedEntity is not { } user) { return; }
+        //Get target item
+        EntityUid? uplinkEntity = null;
+        var entityManager = IoCManager.Resolve<IEntityManager>();
+        //Get TC count
+        var configManager = IoCManager.Resolve<IConfigurationManager>();
+        var tcCount = configManager.GetCVar(CCVars.TraitorStartingBalance);
+        Logger.Debug(entityManager.ToPrettyString(user));
+        //Finally add uplink
+        var uplinkSys = entityManager.EntitySysManager.GetEntitySystem<UplinkSystem>();
+        if (!uplinkSys.AddUplink(user, FixedPoint2.New(tcCount), uplinkEntity: uplinkEntity)) { }
     }
 }
