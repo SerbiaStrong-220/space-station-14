6a4d1b58d1cc962594aef270083cb27e352095a5
Artur Zhidkov <thearturzh@gmail.com>
2023-07-18 05:51:11 +0300

    Added CLA headers

--- Content.Server/SS220/SwitchableWeapon/SwitchableWeaponSystem.cs
+++ Content.Server/SS220/SwitchableWeapon/SwitchableWeaponSystem.cs
@@ -1,3 +1,5 @@
+﻿// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Events;
 using Content.Shared.Examine;
