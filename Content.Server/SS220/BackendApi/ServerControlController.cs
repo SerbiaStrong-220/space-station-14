e5adba77e52d6e05c48b8403f5707067d85a71d7
wer <10843461+Werzet@users.noreply.github.com>
2023-08-13 21:49:11 +1000

    +eula

--- Content.Server/SS220/BackendApi/ServerControlController.cs
+++ Content.Server/SS220/BackendApi/ServerControlController.cs
@@ -1,3 +1,5 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using System.Net.Http;
 using System.Net;
 using System.Threading.Tasks;
