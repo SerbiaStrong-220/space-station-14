dbc92e86792cb54085ebb4ebf8ee714ce43bc48e
CthulhuFhtaghn <142225993+CthulhuFhtaghn@users.noreply.github.com>
2023-09-27 15:31:50 +0300

    Добавление уведомление о потере памяти

--- Content.Server/Mobs/DeathgaspSystem.cs
+++ Content.Server/Mobs/DeathgaspSystem.cs
@@ -1,14 +1,22 @@
 using Content.Server.Chat.Systems;
 using Content.Shared.Mobs;
 using Robust.Shared.Prototypes;
-using Content.Shared.Popups;
+using Content.Server.EUI;
+using Content.Server.Mind;
+using Content.Server.Mind.Components;
+using Robust.Server.Player;
+using Content.Server.SS220.DeathReminder.DeathReminderEui;
+
 
 namespace Content.Server.Mobs;
+
 
 /// <see cref="DeathgaspComponent"/>
 public sealed class DeathgaspSystem: EntitySystem
 {
     [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly EuiManager _euiManager = null!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
 
     public override void Initialize()
     {
@@ -16,14 +24,25 @@ public sealed class DeathgaspSystem: EntitySystem
 
         SubscribeLocalEvent<DeathgaspComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
-    
-    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
-    
+
+
     private void OnMobStateChanged(EntityUid uid, DeathgaspComponent component, MobStateChangedEvent args)
     {
+
         if (args.NewMobState == MobState.Dead)
-            _popupSystem.PopupEntity(Loc.GetString("death-reminder"), uid, uid, PopupType.LargeCaution);
-        
+        {
+            if (TryComp<MindContainerComponent>(uid, out var mindContainer))
+            {
+                if (mindContainer.Mind is { } mind)
+                {
+                    if (mind.UserId != null && _playerManager.TryGetSessionById(mind.UserId.Value, out var client))
+                    {
+                        _euiManager.OpenEui(new DeathReminderEui(), client);
+                    }
+                }
+            }
+        }
+
         // don't deathgasp if they arent going straight from crit to dead
         if (args.NewMobState != MobState.Dead || args.OldMobState != MobState.Critical)
             return;
