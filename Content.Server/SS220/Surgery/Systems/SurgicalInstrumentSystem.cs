e7bcc86b753f27205f737049eaa1358e9105bfbf
Lazzi0706 <lazzikrytskiy0706@gmail.com>
2023-11-03 22:17:52 +0700

    Добавил файлы с клиента

---
+++ Content.Server/SS220/Surgery/Systems/SurgicalInstrumentSystem.cs
@@ -0,0 +1,64 @@
+
+using Content.Server.Body.Systems;
+using Content.Server.Popups;
+using Content.Server.SS220.Surgery.Components;
+using Content.Shared.Body.Components;
+using Content.Shared.Body.Part;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Content.Shared.MedicalScanner;
+using Content.Shared.SS220.Surgery;
+using Content.Shared.SS220.Surgery.Systems;
+using Robust.Server.GameObjects;
+using Robust.Shared.Player;
+
+namespace Content.Server.SS220.Surgery.Systems
+{
+    public sealed partial class SurgicalInstrumentSystem : EntitySystem
+    {
+        [Dependency] private readonly UserInterfaceSystem _uiSystem = default!;
+        [Dependency] private readonly PopupSystem _popup = default!;
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<SurgicalInstrumentComponent, AfterInteractEvent>(OnAfterInteractEvent);
+            SubscribeLocalEvent<SurgicalInstrumentComponent, UseInHandEvent>(SwitchInstrumentMode);
+            SubscribeLocalEvent<SurgicalInstrumentComponent, SelectorButtonPressed>(UpdateTarget);
+        }
+
+        public void SwitchInstrumentMode(EntityUid uid, SurgicalInstrumentComponent component, UseInHandEvent args)
+        {
+            var instrumentPopup = component.Mode == SurgicalInstrumentMode.SELECTOR ? "Выбор зоны операции" : "Оперирование";
+            component.Mode = component.Mode == SurgicalInstrumentMode.SELECTOR ? SurgicalInstrumentMode.OPERATION : SurgicalInstrumentMode.SELECTOR;
+            _popup.PopupEntity(instrumentPopup, args.User);
+        }
+
+        public void OnAfterInteractEvent(EntityUid uid, SurgicalInstrumentComponent component, AfterInteractEvent args)
+        {
+            if (!args.CanReach || args.Target is not {} target)
+                return;
+            if (!TryComp<SurgeonComponent>(target, out var surgeonComponent))
+                return;
+
+            if (component.Mode == SurgicalInstrumentMode.SELECTOR) 
+                PopulateLimbSelector(args.User, target, args.Used);
+        }
+        public void PopulateLimbSelector(EntityUid user, EntityUid target, EntityUid instrument)
+        {
+            if (!TryComp<ActorComponent>(user, out var actor))
+                return;
+
+            var bui = _uiSystem.GetUiOrNull(instrument, LimbSelectorUiKey.Key);
+            if (bui != null)
+            {
+                _uiSystem.OpenUi(bui, actor.PlayerSession);
+                _uiSystem.SendUiMessage(bui, new InstrumentUsedAfterInteractEvent(GetNetEntity(target)));
+            }
+        }
+
+        public void UpdateTarget(EntityUid uid, SurgicalInstrumentComponent component, SelectorButtonPressed msg)
+        {
+            component.Target = GetEntity(msg.LimbId);
+        }
+    }
+}
---
+++ Content.Shared/SS220/Surgery/Systems/SharedSurgerySystem.cs
@@ -0,0 +1,11 @@
+
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.SS220.Surgery
+{
+    [Serializable, NetSerializable]
+    public enum LimbSelectorUiKey : byte
+    {
+        Key
+    }
+}
