e062627dc8d65e5fedc60c86bf5c7b51c61ced41
SkaldetSkaeg <impotekh@gmail.com>
2023-12-03 21:06:00 +0500

    Поправлены тэги (очень странно работает со Storage, так как предмет проверяется и по тэгу, и по компонентам, в результате предмет может вставиться, написав ошибку вайтлиста)

--- Content.Shared/SS220/SmartFridge/SharedSmartFridgeSystem.cs
+++ Content.Shared/SS220/SmartFridge/SharedSmartFridgeSystem.cs
@@ -1,20 +1,5 @@
 using System.Linq;
 using Content.Shared.ActionBlocker;
-using Content.Shared.CombatMode;
-using Content.Shared.Containers.ItemSlots;
-using Content.Shared.Destructible;
-using Content.Shared.DoAfter;
-using Content.Shared.Hands;
-using Content.Shared.Hands.Components;
-using Content.Shared.Hands.EntitySystems;
-using Content.Shared.Implants.Components;
-using Content.Shared.Interaction;
-using Content.Shared.Item;
-using Content.Shared.Lock;
-using Content.Shared.Placeable;
-using Content.Shared.Popups;
-using Content.Shared.Stacks;
-using Content.Shared.Storage.Components;
 namespace Content.Shared.SS220.SmartFridge;
 
 public abstract class SharedSmartFridgeSystem : EntitySystem
