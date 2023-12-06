0bdc3ddd758437d6c628a1af3acb5d7acc5757d0
Andrey Ushakov <10843461+Werzet@users.noreply.github.com>
2023-12-06 23:25:46 +1000

    add rights require for clone

--- Content.Server/Administration/Systems/AdminVerbSystem.cs
+++ Content.Server/Administration/Systems/AdminVerbSystem.cs
@@ -204,26 +204,29 @@ namespace Content.Server.Administration.Systems
                     });
 
                     // Clone - Spawn but without the mind transfer, also spawns at the user's coordinates not the target's
-                    args.Verbs.Add(new Verb()
+                    if (_adminManager.HasAdminFlag(player, AdminFlags.Ban)) 
                     {
-                        Text = Loc.GetString("admin-player-actions-clone"),
-                        Category = VerbCategory.Admin,
-                        Act = () =>
+                        args.Verbs.Add(new Verb()
                         {
-                            if (!_transformSystem.TryGetMapOrGridCoordinates(args.User, out var coords))
+                            Text = Loc.GetString("admin-player-actions-clone"),
+                            Category = VerbCategory.Admin,
+                            Act = () =>
                             {
-                                _popup.PopupEntity(Loc.GetString("admin-player-spawn-failed"), args.User, args.User);
-                                return;
-                            }
+                                if (!_transformSystem.TryGetMapOrGridCoordinates(args.User, out var coords))
+                                {
+                                    _popup.PopupEntity(Loc.GetString("admin-player-spawn-failed"), args.User, args.User);
+                                    return;
+                                }
 
-                            var stationUid = _stations.GetOwningStation(args.Target);
+                                var stationUid = _stations.GetOwningStation(args.Target);
 
-                            var profile = _ticker.GetPlayerProfile(targetActor.PlayerSession);
-                            _spawning.SpawnPlayerMob(coords.Value, null, profile, stationUid);
-                        },
-                        ConfirmationPopup = true,
-                        Impact = LogImpact.High,
-                    });
+                                var profile = _ticker.GetPlayerProfile(targetActor.PlayerSession);
+                                _spawning.SpawnPlayerMob(coords.Value, null, profile, stationUid);
+                            },
+                            ConfirmationPopup = true,
+                            Impact = LogImpact.High,
+                        });
+                    }
                 }
 
                 // Admin Logs
