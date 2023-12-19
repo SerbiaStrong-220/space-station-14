f37866728f36a6f237202000a4b72db7edf31a2f
Lazzi0706 <lazzikrytskiy0706@gmail.com>
2023-12-20 01:05:36 +0700

    Branch migration

---
+++ Content.Shared/SS220/ChemicalImplanter/ChemicalImplanterComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.SS220.ChemicalImplanter
+{
+    [RegisterComponent, NetworkedComponent]
+    public sealed partial class ChemicalImplanterComponent : Component
+    {
+    }
+
+}
