5b68965d0ee253528fb5d363cb94c6934d646662
Lazzi0706 <lazzikrytskiy0706@gmail.com>
2023-12-26 11:02:09 +0700

    Added license

--- Content.Server/Administration/Commands/GarbageClearUpCommand.cs
+++ Content.Server/Administration/Commands/GarbageClearUpCommand.cs
@@ -1,3 +1,5 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using Content.Shared.Administration;
 using Content.Shared.Tag;
 using Robust.Shared.Console;
