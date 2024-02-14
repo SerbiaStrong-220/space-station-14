38eb8da7d4eb3a3715f6dc0c675a4100d67aef01
Skriler <skrilerez@gmail.com>
2024-02-14 23:18:12 +0200

    add smes bound ui

--- Content.Shared/SMES/SharedSmesComponent.cs
+++ Content.Shared/SMES/SharedSmesComponent.cs
@@ -8,3 +8,27 @@ public enum SmesVisuals
     LastChargeState,
     LastChargeLevel,
 }
+
+// SS220 smes-ui-fix begin
+[Serializable, NetSerializable]
+public enum SmesUiKey : byte
+{
+    Key,
+}
+
+[Serializable, NetSerializable]
+public sealed class SmesState : BoundUserInterfaceState
+{
+    public string EntityName { get; }
+    public string DeviceNetworkAddress { get; }
+    public int BatteryChargePercentRounded { get; }
+
+
+    public SmesState(string entityName, string deviceNetworkAddress, int batteryChargePercentRounded)
+    {
+        EntityName = entityName;
+        DeviceNetworkAddress = deviceNetworkAddress;
+        BatteryChargePercentRounded = batteryChargePercentRounded;
+    }
+}
+// SS220 smes-ui-fix end
