e999c37cf774c824a61e63918ddca8153ace7ea3
Gnomeev <137652177+Gnomeev@users.noreply.github.com>
2024-05-01 15:48:34 +0300

    Revert "cultcultmybeloved"
    This reverts commit ba303e71510c9a533b169dc32d7146c7a150ac0b.

--- Content.Server/SS220/AntagItem/AntagStorageComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Server.SS220.AntagItem
-{
-    [RegisterComponent]
-    public sealed partial class AntagStorageComponent : Component
-    {
-        [DataField("slot")]
-        public string Slot = string.Empty;
-    }
-}
