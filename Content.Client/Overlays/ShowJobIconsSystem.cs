6561e70024c52001536baced1f110e72a2463721
Ady4ik <stalcraftexbo@yandex.ru>
2024-06-11 17:35:48 +0300

    System for check is entity have borgchasis component and add JobIconForBorgs

--- Content.Client/Overlays/ShowJobIconsSystem.cs
+++ Content.Client/Overlays/ShowJobIconsSystem.cs
@@ -2,6 +2,7 @@ using Content.Shared.Access.Components;
 using Content.Shared.Access.Systems;
 using Content.Shared.Overlays;
 using Content.Shared.PDA;
+using Content.Shared.Silicons.Borgs.Components;
 using Content.Shared.StatusIcon;
 using Content.Shared.StatusIcon.Components;
 using Robust.Shared.Prototypes;
@@ -15,6 +16,9 @@ public sealed class ShowJobIconsSystem : EquipmentHudSystem<ShowJobIconsComponen
 
     [ValidatePrototypeId<StatusIconPrototype>]
     private const string JobIconForNoId = "JobIconNoId";
+    /// SS220 Add icon for borgs begin
+    private const string JobInconForBorg = "JobIconBorg";
+    /// SS220 Add icon for borgs end
 
     public override void Initialize()
     {
@@ -29,6 +33,13 @@ public sealed class ShowJobIconsSystem : EquipmentHudSystem<ShowJobIconsComponen
             return;
 
         var iconId = JobIconForNoId;
+
+        /// SS220 check if Entity is not borg begin
+        if (HasComp<BorgChassisComponent>(uid))
+        {
+            iconId = JobInconForBorg;
+        }
+        /// SS220 check if Entity is not borg end
 
         if (_accessReader.FindAccessItemsInventory(uid, out var items))
         {
