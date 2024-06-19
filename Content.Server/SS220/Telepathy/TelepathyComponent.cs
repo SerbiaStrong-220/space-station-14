8a100191c0bc1f5336a91824df29c3cfcc4a6023
NailMinnigalin <DisappearedCat@yandex.ru>
2024-06-19 21:02:19 +0300

    Add basic telepathy action

---
+++ Content.Server/SS220/Telepathy/TelepathyComponent.cs
@@ -0,0 +1,16 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Server.SS220.Telepathy;
+
+/// <summary>
+/// This is used for giving telepathy ability
+/// </summary>
+[RegisterComponent]
+public sealed partial class TelepathyComponent : Component
+{
+    [DataField]
+    public EntProtoId Action = "TelepathyAction";
+
+    [DataField]
+    public EntityUid? ActionEntity;
+}
---
+++ Content.Server/SS220/Telepathy/TelepathySystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Server.Actions;
+
+namespace Content.Server.SS220.Telepathy;
+
+/// <summary>
+/// This handles telepathy
+/// </summary>
+public sealed class TelepathySystem : EntitySystem
+{
+    [Dependency] private readonly ActionsSystem _actions = default!;
+    
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<TelepathyComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(EntityUid uid, TelepathyComponent component, MapInitEvent args)
+    {
+        _actions.AddAction(uid, ref component.ActionEntity, component.Action, uid);
+    }
+}
