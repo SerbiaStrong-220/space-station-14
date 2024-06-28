c4dd817b7d5876342800844814ebf161020f7825
NailMinnigalin <DisappearedCat@yandex.ru>
2024-06-28 21:48:11 +0300

    Change message wrapping for telepathy

--- Content.Server/SS220/Telepathy/TelepathySystem.cs
+++ Content.Server/SS220/Telepathy/TelepathySystem.cs
@@ -1,5 +1,6 @@
 ﻿// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
+using Content.Server.Chat.Systems;
 using Content.Shared.Chat;
 using Content.Shared.SS220.Telepathy;
 using Robust.Shared.Network;
@@ -26,35 +27,34 @@ public sealed class TelepathySystem : EntitySystem
 
     private void OnTelepathyAnnouncementSend(EntityUid uid, TelepathyComponent component, TelepathyAnnouncementSendEvent args)
     {
-        SendMessageToEveryoneWithRightChannel(args.TelepathyChannel, args.Message);
+        SendMessageToEveryoneWithRightChannel(args.TelepathyChannel, args.Message, null);
     }
 
-    private void OnTelepathySend(EntityUid uid, TelepathyComponent component, TelepathySendEvent args)
+    private void OnTelepathySend(EntityUid senderUid, TelepathyComponent component, TelepathySendEvent args)
     {
-        if (!HasComp<TelepathyComponent>(uid))
+        if (!HasComp<TelepathyComponent>(senderUid))
             return;
 
-        SendMessageToEveryoneWithRightChannel(component.TelepathyChannelPrototype, args.Message);
+        SendMessageToEveryoneWithRightChannel(component.TelepathyChannelPrototype, args.Message, senderUid);
     }
 
-    private void SendMessageToEveryoneWithRightChannel(string rightTelepathyChanel, string message)
+    private void SendMessageToEveryoneWithRightChannel(string rightTelepathyChanel, string message, EntityUid? senderUid)
     {
         var telepathyQuery = EntityQueryEnumerator<TelepathyComponent>();
         while (telepathyQuery.MoveNext(out var receiverUid, out var receiverTelepathy))
         {
             if (rightTelepathyChanel == receiverTelepathy.TelepathyChannelPrototype)
-                SendMessageToChat(receiverUid, message);
+                SendMessageToChat(receiverUid, message, senderUid);
         }
     }
 
 
-    private void SendMessageToChat(EntityUid uid, string messageString)
+    private void SendMessageToChat(EntityUid receiverUid, string messageString, EntityUid? senderUid)
     {
-        var netSource = _entityManager.GetNetEntity(uid);
-        var wrappedMessage = Loc.GetString(
-            "chat-manager-server-wrap-message",
-            ("message", FormattedMessage.EscapeText(messageString))
-        );
+        var name = GetSenderName(senderUid);
+
+        var netSource = _entityManager.GetNetEntity(receiverUid);
+        var wrappedMessage = GetWrappedTelepathyMessage(receiverUid, messageString, senderUid);
         var message = new ChatMessage(
             ChatChannel.Telepathy,
             messageString,
@@ -62,7 +62,32 @@ public sealed class TelepathySystem : EntitySystem
             netSource,
             null
         );
-        if (TryComp(uid, out ActorComponent? actor))
+        if (TryComp(receiverUid, out ActorComponent? actor))
             _netMan.ServerSendMessage(new MsgChatMessage() {Message = message}, actor.PlayerSession.Channel);
+    }
+
+    private string GetWrappedTelepathyMessage(EntityUid receiverUid, string messageString, EntityUid? senderUid)
+    {
+        if (senderUid == null)
+        {
+            return Loc.GetString(
+                "chat-manager-send-telepathy-announce",
+                ("announce", FormattedMessage.EscapeText(messageString))
+            );
+        }
+
+        return  Loc.GetString(
+            "chat-manager-send-telepathy-message",
+            ("message", FormattedMessage.EscapeText(messageString)),
+            ("senderName", GetSenderName(senderUid))
+        );
+    }
+
+    private string GetSenderName(EntityUid? senderUid)
+    {
+        var nameEv = new TransformSpeakerNameEvent(senderUid!.Value, Name(senderUid.Value));
+        RaiseLocalEvent(senderUid.Value, nameEv);
+        var name = nameEv.Name;
+        return name;
     }
 }
