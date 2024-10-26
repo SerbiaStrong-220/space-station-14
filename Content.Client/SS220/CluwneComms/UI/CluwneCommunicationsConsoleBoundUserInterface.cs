12bd30651d24276da0a3d0d1c74e36218fe6f14c
SkaldetSkaeg <impotekh@gmail.com>
2024-10-26 14:45:25 +0700

    first steps in new systems

---
+++ Content.Client/SS220/CluwneComms/UI/CluwneCommunicationsConsoleBoundUserInterface.cs
@@ -0,0 +1,47 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
+using Content.Shared.CCVar;
+using Content.Shared.Chat;
+using Content.Shared.Communications;
+using Robust.Client.UserInterface;
+using Robust.Shared.Configuration;
+using Robust.Shared.Timing;
+
+namespace Content.Client.SS220.CluwneComms.UI
+{
+    public sealed class CluwneCommunicationsConsoleBoundUserInterface : BoundUserInterface
+    {
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+        [ViewVariables]
+        private CluwneCommunicationsConsoleMenu? _menu;
+
+        public CluwneCommunicationsConsoleBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+        {
+        }
+
+        protected override void Open()
+        {
+            base.Open();
+
+            _menu = this.CreateWindow<CluwneCommunicationsConsoleMenu>();
+            _menu.OnAnnounce += AnnounceButtonPressed;
+        }
+
+        public void AnnounceButtonPressed(string message)
+        {
+            var maxLength = _cfg.GetCVar(CCVars.ChatMaxAnnouncementLength);
+            var msg = SharedChatSystem.SanitizeAnnouncement(message, maxLength);
+            SendMessage(new CommunicationsConsoleAnnounceMessage(msg));
+        }
+
+        protected override void UpdateState(BoundUserInterfaceState state)
+        {
+            base.UpdateState(state);
+
+            if (state is not CommunicationsConsoleInterfaceState commsState)
+                return;
+
+        }
+    }
+}
---
+++ Content.Shared/SS220/CluwneCommunications/SharedCluwneCommunicationsConsoleSystem.cs
@@ -0,0 +1,62 @@
+using Content.Shared.Communications;
+using Robust.Shared.Timing;
+using Robust.Shared.GameObjects;
+using static Content.Shared.Access.Components.AccessOverriderComponent;
+using System.ComponentModel;
+
+namespace Content.Shared.SS220.CluwneCommunications
+{
+    public sealed class SharedCluwneCommunicationssConsoleSystem : EntitySystem
+    {
+        [Dependency] private readonly IGameTiming _timing = default!;
+        [Dependency] private readonly SharedUserInterfaceSystem _uiSystem = default!;
+
+        public override void Initialize()
+        {
+            SubscribeLocalEvent<CluwneCommunicationsConsoleComponent, MapInitEvent>(OnMapInit);
+        }
+        public void OnMapInit(Entity<CluwneCommunicationsConsoleComponent> ent, ref MapInitEvent args)
+        {
+            ent.Comp.AnnouncementCooldownRemaining = _timing.CurTime + ent.Comp.Delay;
+            ent.Comp.AlertCooldownRemaining = _timing.CurTime + ent.Comp.Delay;
+            ent.Comp.CanAnnounce = false;
+            ent.Comp.CanAlert = false;
+        }
+
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            var query = EntityQueryEnumerator<CluwneCommunicationsConsoleComponent>();
+            while (query.MoveNext(out var uid, out var comp))
+            {
+                if (!comp.CanAnnounce && _timing.CurTime >= comp.AnnouncementCooldownRemaining)
+                {
+                    comp.CanAnnounce = true;
+                    UpdateUI(uid, comp);
+                }
+
+                if (!comp.CanAlert && _timing.CurTime >= comp.AlertCooldownRemaining)
+                {
+                    comp.CanAlert = true;
+                    UpdateUI(uid, comp);
+                }
+            }
+        }
+
+        private void UpdateUI(EntityUid ent, CluwneCommunicationsConsoleComponent comp)
+        {
+            CluwneCommunicationsConsoleInterfaceState newState = new CluwneCommunicationsConsoleInterfaceState(
+                comp.CanAnnounce,
+                PrivilegedIdIsAuthorized(uid, component),
+                currentAccess,
+                possibleAccess,
+                missingAccess,
+                privilegedIdName,
+                targetLabel,
+                targetLabelColor);
+
+            _uiSystem.SetUiState(ent, CommunicationsConsoleUiKey.Key, newState);
+        }
+    }
+}
