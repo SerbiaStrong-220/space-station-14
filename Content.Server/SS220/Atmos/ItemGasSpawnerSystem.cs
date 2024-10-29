fac2b5cc5d4813db7a04f74d0bbfa7456dcc29bd
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2024-10-29 21:22:02 +0300

    add licenses

--- Content.Server/SS220/Atmos/ItemGasSpawnerSystem.cs
+++ Content.Server/SS220/Atmos/ItemGasSpawnerSystem.cs
@@ -1,4 +1,4 @@
-
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Server.Atmos.EntitySystems;
 using Content.Shared.Atmos;
 using Robust.Server.GameObjects;
