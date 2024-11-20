9f6148e938c0122d257eda5bc8c7e49f6b600cbd
21Melkuu <79728504+21Melkuu@users.noreply.github.com>
2024-11-20 17:12:45 +0300

    cleanup

--- Content.Shared/SS220/MechFilter/MechFilterComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared.SS220.MechFilter;
-
-/// <summary>
-/// This is used for to determine the mech like a big mechanism
-/// </summary>
-[RegisterComponent]
-public sealed partial class MechFilterComponent : Component
-{
-}
