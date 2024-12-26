248c5111c2c603c9348882a5600feb0c802237dd
SkaldetSkaeg <impotekh@gmail.com>
2024-12-26 23:40:57 +0700

    before merge tweaks

--- Content.Server/Singularity/EntitySystems/EventHorizonSystem.cs
+++ Content.Server/Singularity/EntitySystems/EventHorizonSystem.cs
@@ -8,6 +8,7 @@ using Content.Shared.Mind.Components;
 using Content.Shared.Singularity.Components;
 using Content.Shared.Singularity.EntitySystems;
 using Content.Shared.SS220.CultYogg.Nyarlathotep;
+using Content.Shared.SS220.CultYogg.MiGo;
 using Content.Shared.Tag;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
@@ -46,6 +47,7 @@ public sealed class EventHorizonSystem : SharedEventHorizonSystem
         _physicsQuery = GetEntityQuery<PhysicsComponent>();
 
         SubscribeLocalEvent<NyarlathotepHorizonComponent, EventHorizonAttemptConsumeEntityEvent>(PreventConsume); ///SS220-CultYogg
+        SubscribeLocalEvent<MiGoComponent, EventHorizonAttemptConsumeEntityEvent>(PreventConsume);///SS220-CultYogg
         SubscribeLocalEvent<MapGridComponent, EventHorizonAttemptConsumeEntityEvent>(PreventConsume);
         SubscribeLocalEvent<StationDataComponent, EventHorizonAttemptConsumeEntityEvent>(PreventConsume);
         SubscribeLocalEvent<EventHorizonComponent, MapInitEvent>(OnHorizonMapInit);
