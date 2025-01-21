77d58de12245212cd53274ef787e25c4000c049a
kekw <aronanikushin@gmail.com>
2025-01-21 21:06:31 +0300

    RegalRat personal gamerule

---
+++ Content.Server/SS220/StationEvents/Components/RegalRatRuleComponent.cs
@@ -0,0 +1,19 @@
+﻿using Content.Server.SS220.StationEvents.Events;
+using Content.Server.SS220.StationEvents.Components;
+using Content.Server.StationEvents.Events;
+using Content.Shared.Storage;
+
+namespace Content.Server.SS220.StationEvents.Components;
+
+[RegisterComponent, Access(typeof(RegalRatRule))]
+public sealed partial class RegalRatRuleComponent : Component
+{
+    [DataField("entries")]
+    public List<EntitySpawnEntry> Entries = new();
+
+    /// <summary>
+    /// At least one special entry is guaranteed to spawn
+    /// </summary>
+    [DataField("specialEntries")]
+    public List<EntitySpawnEntry> SpecialEntries = new();
+}
---
+++ Content.Server/SS220/StationEvents/Events/RegalRatSpawnLocation.cs
@@ -0,0 +1,9 @@
+using Content.Server.SS220.StationEvents.Events;
+using Content.Server.SS220.StationEvents.Components;
+using Content.Server.StationEvents.Events;
+
+namespace Content.Server.SS220.StationEvents.Events;
+public sealed partial class RegalRatSpawnLocation : StationEventSystem<RegalRatSpawnLocationComponent>
+{
+
+}
