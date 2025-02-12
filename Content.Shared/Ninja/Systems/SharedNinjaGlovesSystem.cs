abfd528a93d5c59071e87b312ccecec366694e62
Tris0r <163807362+Tris0r@users.noreply.github.com>
2025-02-12 21:24:42 +0500

    Kirus69

--- Content.Shared/Ninja/Systems/SharedNinjaGlovesSystem.cs
+++ Content.Shared/Ninja/Systems/SharedNinjaGlovesSystem.cs
@@ -9,7 +9,6 @@ using Content.Shared.Item.ItemToggle.Components;
 using Content.Shared.Ninja.Components;
 using Content.Shared.Popups;
 using Robust.Shared.Timing;
-using Content.Shared.Actions;
 
 namespace Content.Shared.Ninja.Systems;
 
@@ -24,7 +23,6 @@ public abstract class SharedNinjaGlovesSystem : EntitySystem
     [Dependency] private readonly ItemToggleSystem _toggle = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SharedSpaceNinjaSystem _ninja = default!;
-    [Dependency] private readonly SharedActionsSystem _actions = default!;
 
     public override void Initialize()
     {
@@ -103,13 +101,6 @@ public abstract class SharedNinjaGlovesSystem : EntitySystem
         else
             DisableGloves(ent);
 
-        // SS220 ninja gloves toggle on states start
-        if (!TryComp<ToggleClothingComponent>(ent.Owner, out var toggle))
-            return;
-
-        if (toggle.ActionEntity != null)
-            _actions.SetToggled(toggle.ActionEntity.Value, args.Activated);
-        // // SS220 ninja gloves toggle on states end
     }
 
     protected virtual void EnableGloves(Entity<NinjaGlovesComponent> ent, Entity<SpaceNinjaComponent> user)
