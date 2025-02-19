f2a7e16002794db464ccb5fbc3e17ea4e6df6532
ReeZer2 <omfgdamndude.dude@gmail.com>
2025-02-19 21:34:57 +0200

    fix commit

---
+++ Content.Shared/SS220/AirlockVisuals/AirlockVisualsComponent.cs
@@ -0,0 +1,13 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.SS220.AirlockVisuals;
+
+/// <summary>
+/// This component used for our airlock visuals
+/// </summary>
+[RegisterComponent]
+[NetworkedComponent]
+public sealed partial class AirlockVisualsComponent : Component
+{
+
+}
