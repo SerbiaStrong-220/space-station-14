543d10e316167447a166aec6281552e6c95d0c1b
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-04-13 19:17:20 +0300

    fix cloning

--- Content.Server/SS220/LimitationRevive/LimitationReviveSystem.cs
+++ Content.Server/SS220/LimitationRevive/LimitationReviveSystem.cs
@@ -1,4 +1,6 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+using Content.Server.StationEvents.Events;
+using Content.Shared.Cloning;
 using Content.Shared.Damage;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Systems;
@@ -8,6 +10,7 @@ using Content.Shared.Rejuvenate;
 using Content.Shared.Traits;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
+using Robust.Shared.Serialization.Manager;
 using Timer = Robust.Shared.Timing.Timer;
 
 namespace Content.Server.SS220.LimitationRevive;
@@ -21,11 +24,14 @@ public sealed class LimitationReviveSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _prototype = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly EntityManager _entityManager = default!;
+    [Dependency] private readonly ISerializationManager _serialization = default!;
+
     /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<LimitationReviveComponent, UpdateMobStateEvent>(OnDeadMobState);
         SubscribeLocalEvent<LimitationReviveComponent, RejuvenateEvent>(OnUseAdminCommand);
+        SubscribeLocalEvent<LimitationReviveComponent, CloningEvent>(OnCloning);
     }
 
     private void OnDeadMobState(Entity<LimitationReviveComponent> ent, ref UpdateMobStateEvent args)
@@ -47,6 +53,16 @@ public sealed class LimitationReviveSystem : EntitySystem
         ent.Comp.IsDamageTaken = false;
         ent.Comp.IsAlreadyDead = false;
         ent.Comp.CounterOfDead = 0;
+    }
+
+    private void OnCloning(Entity<LimitationReviveComponent> entity, ref CloningEvent args)
+    {
+        var targetComp = EnsureComp<LimitationReviveComponent>(args.Target);
+        _serialization.CopyTo(entity.Comp, ref targetComp, notNullableOverride: true);
+
+        targetComp.IsDamageTaken = false;
+        targetComp.IsAlreadyDead = false;
+        targetComp.CounterOfDead = 0;
     }
 
     /// <summary>
