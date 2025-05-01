fc5981dec23acb5e2184b8e0d14c009976132092
ReeZer2 <omfgdamndude.dude@gmail.com>
2025-05-01 22:51:21 +0300

    ADD: Transfer Items in storage

---
+++ Content.Shared/SS220/TransferItemsInStorage/TransferItemsInStorageComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.SS220.TransferItemsInStorage;
+
+[RegisterComponent]
+[NetworkedComponent]
+public sealed partial class TransferItemsInStorageComponent : Component
+{
+
+}
