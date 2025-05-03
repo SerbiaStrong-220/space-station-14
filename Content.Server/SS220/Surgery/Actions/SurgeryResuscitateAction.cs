ebdfaa410ac88e7e4228e0bb0f7909f59e9dbf03
Anri <elyasin.ane@mail.ru>
2025-05-03 20:15:47 +0300

    post-mm-fixes

--- Content.Server/SS220/Surgery/Actions/SurgeryResuscitateAction.cs
+++ Content.Server/SS220/Surgery/Actions/SurgeryResuscitateAction.cs
@@ -1,6 +1,5 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
-using Content.Server.Surgery.ActionSystems;
 using Content.Shared.Damage;
 using Content.Shared.SS220.Surgery.Graph;
 
@@ -14,6 +13,9 @@ public sealed partial class SurgeryResuscitateAction : ISurgeryGraphAction
 
     public void PerformAction(EntityUid uid, EntityUid? userUid, EntityUid? used, IEntityManager entityManager)
     {
-        entityManager.System<SurgeryResuscitateSystem>().Resuscitate(uid, userUid, Heal);
+        // TODO
+        // We have fuckup-likish defib system where all comes in, so copy-paste could be a headache
+        // Idea is to make defibish thing to make a good work around. Hope Ill have time
+        // entityManager.System<SurgeryResuscitateSystem>().Resuscitate(uid, userUid, Heal);
     }
 }
