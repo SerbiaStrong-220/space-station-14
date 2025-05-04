55cdd653a29dd8dce2c7d801b7e9bf68f1d08e3d
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-05-04 22:22:30 +0300

    Add logs on movement mode changed

--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
+using Content.Shared.Administration.Logs;
 using Content.Shared.Bed.Sleep;
 using Content.Shared.CCVar;
 using Content.Shared.Friction;
