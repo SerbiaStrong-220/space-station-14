8fe193bbffb5e463dde946c27beae8266ad02771
Roman <nice@corp.com>
2025-05-05 18:55:10 +0300

    add license

--- Content.Server/SS220/SuicideByGun/SuicideByGunSystem.cs
+++ Content.Server/SS220/SuicideByGun/SuicideByGunSystem.cs
@@ -1,3 +1,5 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using Content.Server.DoAfter;
 using Content.Server.Popups;
 using Content.Shared.Damage;
@@ -17,6 +19,8 @@ public sealed partial class SuicideByGunSystem : EntitySystem
 
     public override void Initialize()
     {
+        base.Initialize();
+
         SubscribeLocalEvent<SuicideByGunComponent, GetVerbsEvent<Verb>>(OnGetVerbs);
         SubscribeLocalEvent<SuicideDoAfterEvent>(OnDoAfterComplete);
     }
