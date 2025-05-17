66285689bab70d5e87c43db4720f56c86619b693
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-05-17 22:48:39 +0300

    add some API

--- Content.Shared/SS220/Zones/Systems/SharedZonesSystem.cs
+++ Content.Shared/SS220/Zones/Systems/SharedZonesSystem.cs
@@ -1,18 +1,16 @@
 
+using Content.Shared.Maps;
 using Content.Shared.SS220.Zones.Components;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Dynamics;
 
 namespace Content.Shared.SS220.Zones.Systems;
 
 public abstract partial class SharedZonesSystem : EntitySystem
 {
     [Dependency] private readonly SharedMapSystem _map = default!;
-    [Dependency] private readonly SharedTransformSystem _transform = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-
-    }
 
     public int? GetZoneId(Entity<ZoneComponent> zone)
     {
@@ -33,6 +31,49 @@ public abstract partial class SharedZonesSystem : EntitySystem
         }
 
         return null;
+    }
+
+    public bool InZone(EntityUid entity, EntityUid zoneUid)
+    {
+        var zones = ZonesByCoordinates(Transform(entity).Coordinates);
+        foreach (var zone in zones)
+        {
+            if (zone.ZoneEntity == GetNetEntity(zoneUid))
+                return true;
+        }
+
+        return false;
+    }
+
+    public IEnumerable<ZoneData> ZonesByCoordinates(EntityCoordinates coordinates)
+    {
+        List<ZoneData> zones = new();
+        var grid = coordinates.EntityId;
+        if (!TryComp<MapGridComponent>(grid, out var mapGrid) ||
+            !TryComp<ZonesDataComponent>(grid, out var zoneData))
+            return zones;
+
+        return ZonesByCoordinates((grid, mapGrid, zoneData), coordinates);
+    }
+
+    public IEnumerable<ZoneData> ZonesByCoordinates(Entity<MapGridComponent?, ZonesDataComponent?> grid, EntityCoordinates coordinates)
+    {
+        List<ZoneData> zones = new();
+        if (!Resolve(grid, ref grid.Comp1) ||
+            !Resolve(grid, ref grid.Comp2))
+            return zones;
+
+        var tileRef = _map.GetTileRef((grid, grid.Comp1), coordinates);
+        if (tileRef.IsSpace())
+            return zones;
+
+        foreach (var zone in grid.Comp2.Zones.Values)
+        {
+            if (zone.Tiles.Contains(tileRef.GridIndices))
+                zones.Add(zone);
+        }
+
+        return zones;
     }
 
     public ZoneData? GetZoneData(Entity<ZoneComponent> zone)
@@ -58,5 +99,49 @@ public abstract partial class SharedZonesSystem : EntitySystem
     {
         grid.Comp.Zones.TryGetValue(zoneId, out var zoneData);
         return zoneData;
+    }
+
+    public IEnumerable<EntityUid> GetEntitiesInZone(Entity<MapGridComponent, ZonesDataComponent> grid, int zoneId)
+    {
+        HashSet<EntityUid> entities = new();
+        if (!TryComp<BroadphaseComponent>(grid, out var broadphase))
+            return entities;
+
+        var zone = GetZoneData((grid, grid), zoneId);
+        if (zone == null)
+            return entities;
+
+        foreach (var tile in zone.Tiles)
+            GetEntitiesInTile((grid, broadphase, grid), tile, ref entities);
+
+        return entities;
+    }
+
+    private void GetEntitiesInTile(
+        Entity<BroadphaseComponent, MapGridComponent> grid,
+        Vector2i tile,
+        ref HashSet<EntityUid> processed)
+    {
+        var lookup = grid.Comp1;
+        var size = grid.Comp2.TileSize;
+        var tileBox = new Box2(tile * size, (tile + 1) * size);
+
+        var state = processed;
+
+        lookup.DynamicTree.QueryAabb(ref state, ZoneQueryCallback, tileBox, true);
+        lookup.StaticTree.QueryAabb(ref state, ZoneQueryCallback, tileBox, true);
+        lookup.SundriesTree.QueryAabb(ref state, ZoneQueryCallback, tileBox, true);
+        lookup.StaticSundriesTree.QueryAabb(ref state, ZoneQueryCallback, tileBox, true);
+    }
+
+    private static bool ZoneQueryCallback(ref HashSet<EntityUid> processed, in EntityUid uid)
+    {
+        processed.Add(uid);
+        return true;
+    }
+
+    private static bool ZoneQueryCallback(ref HashSet<EntityUid> processed, in FixtureProxy proxy)
+    {
+        return ZoneQueryCallback(ref processed, proxy.Entity);
     }
 }
