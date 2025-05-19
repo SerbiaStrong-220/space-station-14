c0224ae637757c1f4c563b3444f3d8d7d6c04685
Oxygen033 <moormaxim2k4@gmail.com>
2025-05-19 20:48:49 +0600

    m

--- Content.Server/SS220/ConditionalEmotes/ConditionalEmotesSystem.cs
+++ Content.Server/SS220/ConditionalEmotes/ConditionalEmotesSystem.cs
@@ -2,7 +2,10 @@
 
 using Content.Server.Body.Components;
 using Content.Server.Chat.Systems;
+using Content.Server.Explosion.Components;
 using Content.Server.Temperature.Components;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
 using Content.Shared.Temperature;
 
 namespace Content.Server.SS220.ConditionalEmotes;
@@ -15,32 +18,38 @@ public sealed class ConditionalEmotesSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<ConditionalEmotesComponent, OnTemperatureChangeEvent>(OnTemperatureChanged);
+        SubscribeLocalEvent<ConditionalEmotesComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
 
     private void OnTemperatureChanged(Entity<ConditionalEmotesComponent> ent, ref OnTemperatureChangeEvent args)
     {
-        if (!TryComp<TemperatureComponent>(ent, out var comp1) || !TryComp<ThermalRegulatorComponent>(ent, out var comp2))
+        if (!TryComp<TemperatureComponent>(ent, out var _) || !TryComp<ThermalRegulatorComponent>(ent, out var comp))
             return;
 
-        if (!ent.Comp.IsCold && args.CurrentTemperature < comp2.NormalBodyTemperature)
+        if (!ent.Comp.IsCold && args.CurrentTemperature < comp.NormalBodyTemperature)
         {
             ent.Comp.IsCold = true;
             _autoEmote.AddEmote(ent.Owner, "LowTempShake");
         }
-        else if (ent.Comp.IsCold && args.CurrentTemperature > comp2.NormalBodyTemperature)
+        else if (ent.Comp.IsCold && args.CurrentTemperature > comp.NormalBodyTemperature)
         {
             ent.Comp.IsCold = false;
             _autoEmote.RemoveEmote(ent.Owner, "LowTempShake", null, false);
         }
-        if (!ent.Comp.IsHot && args.CurrentTemperature > comp2.NormalBodyTemperature)
+        if (!ent.Comp.IsHot && args.CurrentTemperature > comp.NormalBodyTemperature)
         {
             ent.Comp.IsHot = true;
             _autoEmote.AddEmote(ent.Owner, "HighTempSweat");
         }
-        else if (ent.Comp.IsHot && args.CurrentTemperature < comp2.NormalBodyTemperature)
+        else if (ent.Comp.IsHot && args.CurrentTemperature < comp.NormalBodyTemperature)
         {
             ent.Comp.IsHot = false;
             _autoEmote.RemoveEmote(ent.Owner, "HighTempSweat", null, false);
         }
+    }
+
+    private void OnMobStateChanged(Entity<ConditionalEmotesComponent> ent, ref MobStateChangedEvent args)
+    {
+
     }
 }
