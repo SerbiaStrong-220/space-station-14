2e6333d1c3525be255884504487132f9b6e4120e
21Melkuu <79728504+21Melkuu@users.noreply.github.com>
2025-05-25 18:25:36 +0300

    add: TrapSystem

--- Content.Shared/SS220/CultYogg/FruitTrap/CultYoggTrapComponent.cs
+++ Content.Shared/SS220/CultYogg/FruitTrap/CultYoggTrapComponent.cs
@@ -4,7 +4,7 @@ namespace Content.Shared.SS220.CultYogg.FruitTrap;
 /// This is used for...
 /// </summary>
 [RegisterComponent]
-public sealed partial class CultYoggItemFruitTrapComponent : Component
+public sealed partial class CultYoggTrapComponent : Component
 {
     /// <summary>
     /// if 0 = blalal
