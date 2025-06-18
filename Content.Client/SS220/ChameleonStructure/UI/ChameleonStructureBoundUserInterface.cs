8cc769fe931e5ba45ef68290562d09512cd01b01
SkaldetSkaeg <impotekh@gmail.com>
2025-06-18 12:50:10 +0700

    some fixes but idk

--- Content.Client/SS220/ChameleonStructure/UI/ChameleonStructureBoundUserInterface.cs
+++ Content.Client/SS220/ChameleonStructure/UI/ChameleonStructureBoundUserInterface.cs
@@ -5,14 +5,12 @@ using JetBrains.Annotations;
 using Robust.Client.UserInterface;
 using Robust.Shared.Prototypes;
 using Content.Shared.SS220.ChameleonStructure;
-using Content.SS220.ChameleonStructure.UI;
 
 namespace Content.Client.SS220.ChameleonStructure.UI;
 
 [UsedImplicitly]
 public sealed class ChameleonStructureBoundUserInterface : BoundUserInterface
 {
-    [Dependency] private readonly IComponentFactory _factory = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
     private readonly ChameleonStructureSystem _chameleon;
     private readonly TagSystem _tag;
@@ -40,6 +38,27 @@ public sealed class ChameleonStructureBoundUserInterface : BoundUserInterface
 
         if (state is not ChameleonStructureBoundUserInterfaceState st)
             return;
+
+        var targets = _chameleon.GetValidTargets();
+
+        if (st.RequiredTag == null)
+        {
+            _menu?.UpdateState(targets, st.SelectedId);
+            return;
+        }
+
+        var newTargets = new List<EntProtoId>();
+        foreach (var target in targets)
+        {
+            if (string.IsNullOrEmpty(target) || !_proto.TryIndex(target, out EntityPrototype? proto))
+                continue;
+
+            if (!proto.TryGetComponent(out TagComponent? tag, EntMan.ComponentFactory) || !_tag.HasTag(tag, st.RequiredTag))
+                continue;
+
+            newTargets.Add(target);
+        }
+        _menu?.UpdateState(newTargets, st.SelectedId);
     }
 
     private void OnIdSelected(string selectedId)
--- Content.Shared/SS220/ChameleonStructure/SharedChameleonStructureSystem.cs
+++ Content.Shared/SS220/ChameleonStructure/SharedChameleonStructureSystem.cs
@@ -1,12 +1,17 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
 using Content.Shared.Clothing.Components;
+using Content.Shared.Clothing.EntitySystems;
+using Content.Shared.Contraband;
 using Content.Shared.Inventory;
+using Content.Shared.Item;
 using Content.Shared.Tag;
 using Content.Shared.Verbs;
 using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 using Robust.Shared.Utility;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Content.Shared.SS220.ChameleonStructure;
 
@@ -14,20 +19,36 @@ public abstract class SharedChameleonStructureSystem : EntitySystem
 {
     [Dependency] protected readonly SharedUserInterfaceSystem UI = default!;
     [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
+    [Dependency] private readonly ClothingSystem _clothingSystem = default!;
+    [Dependency] private readonly ContrabandSystem _contraband = default!;
+    [Dependency] private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly SharedItemSystem _itemSystem = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly TagSystem _tag = default!;
+    [Dependency] protected readonly IGameTiming _timing = default!;
+
+    private readonly List<EntProtoId> _data = new();
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<ChameleonStructureComponent, GetVerbsEvent<InteractionVerb>>(OnVerb);
+        SubscribeLocalEvent<ChameleonStructureComponent, PrototypesReloadedEventArgs>(OnPrototypeReload);
+        PrepareAllVariants();
     }
 
+    private void OnPrototypeReload(Entity<ChameleonStructureComponent> ent, ref PrototypesReloadedEventArgs args)
+    {
+        PrepareAllVariants();
+    }
     private void OnVerb(Entity<ChameleonStructureComponent> ent, ref GetVerbsEvent<InteractionVerb> args)
     {
         if (!args.CanAccess || !args.CanInteract)
             return;
 
-        if (ent.Comp.UserWhitelist is not null && !_whitelist.IsValid(ent.Comp.UserWhitelist, args.User))
+        if (_whitelist.IsWhitelistFail(ent.Comp.UserWhitelist, args.User))
             return;
 
         // Can't pass args from a ref event inside of lambdas
@@ -44,6 +65,12 @@ public abstract class SharedChameleonStructureSystem : EntitySystem
 
     protected void UpdateVisuals(Entity<ChameleonStructureComponent> ent)
     {
+        if (string.IsNullOrEmpty(ent.Comp.Default) ||
+    !_proto.TryIndex(ent.Comp.Default, out EntityPrototype? proto))
+            return;
+
+        UpdateSprite(ent, proto);
+
     }
 
     /// <summary>
@@ -51,6 +78,39 @@ public abstract class SharedChameleonStructureSystem : EntitySystem
     /// </summary>
     public bool IsValidTarget(EntityPrototype proto, string? requiredTag = null)
     {
+        if (proto.Abstract || proto.HideSpawnMenu)
+            return false;
+
+        if (!proto.TryGetComponent(out TagComponent? tag, Factory))//IDK about WhitelistChameleon should it be or not
+            return false;
+
+        if (requiredTag != null && !_tag.HasTag(tag, requiredTag))
+            return false;
+
         return true;
     }
+
+    /// <summary>
+    ///     Get a list of valid chameleon targets
+    /// </summary>
+    public IEnumerable<EntProtoId> GetValidTargets()
+    {
+        return _data;
+    }
+
+    protected void PrepareAllVariants()
+    {
+        _data.Clear();
+        var prototypes = _proto.EnumeratePrototypes<EntityPrototype>();
+
+        foreach (var proto in prototypes)
+        {
+            // check if this is valid clothing
+            if (!IsValidTarget(proto))
+                continue;
+
+            _data.Add(proto.ID);
+        }
+    }
+
 }
