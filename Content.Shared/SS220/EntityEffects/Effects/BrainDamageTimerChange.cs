e5602680ad8f649cd94b2aeb0563089862ea58e6
SkaldetSkaeg <impotekh@gmail.com>
2025-07-16 16:57:08 +0700

    Update Content.Shared/SS220/EntityEffects/Effects/BrainDamageTimerChange.cs
    Co-authored-by: Kirus59 <145689588+Kirus59@users.noreply.github.com>

--- Content.Shared/SS220/EntityEffects/Effects/BrainDamageTimerChange.cs
+++ Content.Shared/SS220/EntityEffects/Effects/BrainDamageTimerChange.cs
@@ -35,9 +35,9 @@ public sealed partial class BrainDamageTimerChange : EntityEffect
 
         var mod = 1f;//buffer in case we don't have a modifier
 
-        if (args.EntityManager.TryGetComponent<ChemicalAdaptationComponent>(args.TargetEntity, out var adaptComp))
-            if (adaptComp.ChemicalAdaptations.TryGetValue(reagentArgs.Reagent.ID, out var value))
-                mod = value.Modifier;
+        if (args.EntityManager.TryGetComponent<ChemicalAdaptationComponent>(args.TargetEntity, out var adaptComp) &&
+            adaptComp.ChemicalAdaptations.TryGetValue(reagentArgs.Reagent.ID, out var value))
+	    mod = value.Modifier;
 
         limitComp.DamageTime += ProlongedTime * mod;//not sure, maybe it should be function. but then you'll have to move a lot to shared...
     }
