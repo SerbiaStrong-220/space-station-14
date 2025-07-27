4a1ef149b29d4a98e44ba1707cd41755b3619970
SkaldetSkaeg <impotekh@gmail.com>
2025-07-27 23:40:58 +0700

    MiGo Revive UI Fix

--- Content.Server/SS220/EntityEffects/EntityEffectSystem.SS220.cs
+++ Content.Server/SS220/EntityEffects/EntityEffectSystem.SS220.cs
@@ -14,7 +14,7 @@ public sealed partial class EntityEffectSystem : EntitySystem
         SubscribeLocalEvent<CheckEntityEffectConditionEvent<HasComponentsCondition>>(OnCheckComponentCondition);
 
         SubscribeLocalEvent<ExecuteEntityEffectEvent<ChemElixirOfLiberationEffect>>(OnExecuteChemElixirOfLiberation);
-        SubscribeLocalEvent<ExecuteEntityEffectEvent<ChemMiGomyceliumEffect>>(OnExecuteChemMyGomicelium);
+        SubscribeLocalEvent<ExecuteEntityEffectEvent<ChemMiGomyceliumEffect>>(OnExecuteChemMiGomycelium);
         SubscribeLocalEvent<ExecuteEntityEffectEvent<ChemRemoveHallucinationsEffect>>(OnExecuteChemRemoveHallucination);
         SubscribeLocalEvent<ExecuteEntityEffectEvent<SaintWaterDrinkEffect>>(OnExecuteSaintWaterDrinkEffect);
     }
