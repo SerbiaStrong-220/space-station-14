ab40a4104846c22672996ca8e4662b8f0c30e293
krevetkqqq <161033382+krevetkqqq@users.noreply.github.com>
2025-08-15 21:14:01 +0300

    Update Content.Server/SS220/HealOnCollide/BloodstreamExtensionSystem.cs
    Co-authored-by: Kirus59 <145689588+Kirus59@users.noreply.github.com>

--- Content.Server/SS220/HealOnCollide/BloodstreamExtensionSystem.cs
+++ Content.Server/SS220/HealOnCollide/BloodstreamExtensionSystem.cs
@@ -6,6 +6,7 @@ namespace Content.Server.SS220.HealOnCollide.Bloodstream;
 public sealed partial class BloodstreamExtensionSystem : SharedBloodstreamExtensionSystem
 {
     [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
+
     public override void TryModifyBleedAmount(EntityUid uid, float amount)
     {
         _bloodstream.TryModifyBleedAmount(uid, amount);
