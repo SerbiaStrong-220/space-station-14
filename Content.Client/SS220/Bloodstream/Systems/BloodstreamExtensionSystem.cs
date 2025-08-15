b23c3bda996fa69f607584880da14bd98ed67dc4
krevetkqqq <antosinoftenksvov@gmail.com>
2025-08-15 21:08:10 +0300

    .

---
+++ Content.Client/SS220/Bloodstream/Systems/BloodstreamExtensionSystem.cs
@@ -0,0 +1,5 @@
+using Content.Shared.SS220.HealOnCollide.Bloodstream;
+
+namespace Content.Client.SS220.Bloodstream.Systems;
+
+public sealed partial class BloodstreamExtensionSystem : SharedBloodstreamExtensionSystem { public override void TryModifyBleedAmount(EntityUid uid, float amount) { } }
