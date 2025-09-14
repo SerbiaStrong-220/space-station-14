1e5a44f6f4b91465482d7ab9a37ec72ab6470c24
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-09-14 18:37:22 +0300

    wtf

--- Content.Shared/SS220/Language/Components/LanguageComponent.cs
+++ Content.Shared/SS220/Language/Components/LanguageComponent.cs
@@ -1,6 +1,5 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Shared.SS220.Language.Systems;
-using Pidgin.Expression;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
