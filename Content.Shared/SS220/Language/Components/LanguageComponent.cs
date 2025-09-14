fbee230809326b3058d4e14c13dfb49349fb600b
Kirus59 <145689588+Kirus59@users.noreply.github.com>
2025-09-14 18:24:54 +0300

    cognizine languages fix

--- Content.Shared/SS220/Language/Components/LanguageComponent.cs
+++ Content.Shared/SS220/Language/Components/LanguageComponent.cs
@@ -1,5 +1,6 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Shared.SS220.Language.Systems;
+using Pidgin.Expression;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
@@ -26,7 +27,7 @@ public sealed partial class LanguageComponent : Component
     ///  List of languages that the Entity can speak.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public List<LanguageDefinition> AvailableLanguages = new();
+    public HashSet<LanguageDefinition> AvailableLanguages = [];
 
     [DataField, AutoNetworkedField]
     public bool KnowAllLanguages;
@@ -34,7 +35,7 @@ public sealed partial class LanguageComponent : Component
 
 [DataDefinition]
 [Serializable, NetSerializable]
-public sealed partial class LanguageDefinition
+public sealed partial class LanguageDefinition : IEquatable<LanguageDefinition>
 {
     [DataField(required: true)]
     public ProtoId<LanguagePrototype> Id;
@@ -48,8 +49,45 @@ public sealed partial class LanguageDefinition
         CanSpeak = canSpeak;
     }
 
-    public bool IsEqual(LanguageDefinition other)
+    public bool Equals(LanguageDefinition? other)
     {
-        return Id == other.Id;
+        if (other is null)
+            return false;
+
+        return Id.Equals(other.Id);
+    }
+
+    public static bool Equals(LanguageDefinition? left, LanguageDefinition? right)
+    {
+        if (ReferenceEquals(left, right))
+            return true;
+
+        if (left is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator ==(LanguageDefinition? left, LanguageDefinition? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(LanguageDefinition? left, LanguageDefinition? right)
+    {
+        return !Equals(left, right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not LanguageDefinition other)
+            return false;
+
+        return Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
     }
 }
