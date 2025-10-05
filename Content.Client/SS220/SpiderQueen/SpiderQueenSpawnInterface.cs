e2ea6f535c7b385062fed80baa6a612360691ae8
kekw <aronanikushin@gmail.com>
2025-10-05 13:11:38 +0300

    init-commit

---
+++ Content.Client/SS220/SpiderQueen/SpiderQueenSpawnInterface.cs
@@ -0,0 +1,22 @@
+using Content.Client.SS220.GhostHearing;
+using JetBrains.Annotations;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.SS220.SpiderQueenInterface;
+
+[UsedImplicitly]
+public sealed partial class SpiderQueenSpawnInterface : BoundUserInterface
+{
+    private SpiderQueenSpawnWindow? _window;
+
+    public SpiderQueenSpawnInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = this.CreateWindow<SpiderQueenSpawnWindow>();
+    }
+}
