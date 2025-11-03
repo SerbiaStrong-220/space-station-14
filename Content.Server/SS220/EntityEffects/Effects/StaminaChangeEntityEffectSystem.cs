5b5c645d48b7983bd3a64d536a9a12d32af55640
Anri <elyasin.ane@mail.ru>
2025-11-03 15:31:00 +0300

    if-only-refactors-wasnt-so-like-refactors

--- Content.Server/SS220/EntityEffects/Effects/StaminaChangeEntityEffectSystem.cs
+++ Content.Server/SS220/EntityEffects/Effects/StaminaChangeEntityEffectSystem.cs
@@ -1,15 +1,22 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
+using Content.Server.Damage.Systems;
 using Content.Shared.Damage.Components;
-using Content.Shared.Damage.Systems;
 using Content.Shared.EntityEffects;
+using Robust.Shared.Random;
 
 namespace Content.Shared.SS220.EntityEffects.Effects;
 
 public sealed partial class StaminaDamageEntityEffectSystem : EntityEffectSystem<StaminaComponent, StaminaChange>
 {
+    [Dependency] private readonly StaminaSystem _stamina = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
     protected override void Effect(Entity<StaminaComponent> entity, ref EntityEffectEvent<StaminaChange> args)
     {
-        var stunSys = EntityManager.System<SharedStaminaSystem>();
-        stunSys.TakeStaminaDamage(entity, args.Effect.Value, visual: false, ignoreResist: true);
+        if (!_random.Prob(args.Effect.Probability))
+            return;
+
+        _stamina.TakeStaminaDamage(entity, args.Effect.Value, visual: false, ignoreResist: true);
     }
 }
