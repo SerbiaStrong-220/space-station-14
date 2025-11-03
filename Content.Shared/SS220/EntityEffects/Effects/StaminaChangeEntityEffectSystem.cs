b7ad1d45e31e9338d0505f046d6fd7dffe943c62
Anri <elyasin.ane@mail.ru>
2025-11-03 14:32:51 +0300

    frontier-stamina-damage-chance-fix

--- Content.Shared/SS220/EntityEffects/Effects/StaminaChangeEntityEffectSystem.cs
+++ Content.Shared/SS220/EntityEffects/Effects/StaminaChangeEntityEffectSystem.cs
@@ -2,13 +2,12 @@
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Systems;
 using Content.Shared.EntityEffects;
-using Robust.Shared.Prototypes;
 
 namespace Content.Shared.SS220.EntityEffects.Effects;
 
-public sealed partial class StaminaDamageEntityEffectSystem : EntityEffectSystem<StaminaComponent, StaminaDamage>
+public sealed partial class StaminaDamageEntityEffectSystem : EntityEffectSystem<StaminaComponent, StaminaChange>
 {
-    protected override void Effect(Entity<StaminaComponent> entity, ref EntityEffectEvent<StaminaDamage> args)
+    protected override void Effect(Entity<StaminaComponent> entity, ref EntityEffectEvent<StaminaChange> args)
     {
         var stunSys = EntityManager.System<SharedStaminaSystem>();
         stunSys.TakeStaminaDamage(entity, args.Effect.Value, visual: false, ignoreResist: true);
