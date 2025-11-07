26cc6b7e73450e14be826aabb34dc59fbefc96af
SkaldetSkaeg <impotekh@gmail.com>
2025-11-07 23:12:51 +0700

    + not working teleport logic

--- Content.Shared/SS220/TeleportationChasm/TeleportationChasmSystem.cs
+++ Content.Shared/SS220/TeleportationChasm/TeleportationChasmSystem.cs
@@ -1,11 +1,18 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
 using Content.Shared.ActionBlocker;
+using Content.Shared.Labels.Components;
 using Content.Shared.Movement.Events;
+using Content.Shared.SS220.Photocopier.Forms;
+using Content.Shared.Station.Components;
 using Content.Shared.StepTrigger.Systems;
+using Content.Shared.Storage;
+using Content.Shared.Tiles;
 using Content.Shared.Weapons.Misc;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Map;
 using Robust.Shared.Network;
+using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
 namespace Content.Shared.SS220.TeleportationChasm;
@@ -20,6 +27,8 @@ public sealed class TeleportationChasmSystem : EntitySystem
     [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedGrapplingGunSystem _grapple = default!;
+    [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
 
     public override void Initialize()
     {
@@ -38,13 +47,22 @@ public sealed class TeleportationChasmSystem : EntitySystem
         if (_net.IsClient)
             return;
 
+        List<EntityUid> toRemove = [];
+
         var query = EntityQueryEnumerator<TeleportationChasmFallingComponent>();
         while (query.MoveNext(out var uid, out var chasm))
         {
             if (_timing.CurTime < chasm.NextDeletionTime)
                 continue;
 
-            QueueDel(uid);
+            TeleportToRandomLocation(uid);
+
+            toRemove.Add(uid);
+        }
+
+        foreach (var uid in toRemove)
+        {
+            RemComp<TeleportationChasmFallingComponent>(uid);
         }
     }
 
@@ -82,5 +100,31 @@ public sealed class TeleportationChasmSystem : EntitySystem
     private void OnUpdateCanMove(Entity<TeleportationChasmFallingComponent> ent, ref UpdateCanMoveEvent args)
     {
         args.Cancel();
+    }
+
+    private void TeleportToRandomLocation(EntityUid ent)
+    {
+        var validLocations = new List<EntityCoordinates>();
+
+        //var locations = EntityQueryEnumerator<FloorTileComponent, TransformComponent>();//I think only objects has tranform, so i mybe shoul pick smth else
+        //while (locations.MoveNext(out _, out _, out var transform))
+        //{
+        //    //ToDo_SS220 figure out station check
+        //    validLocations.Add(transform.Coordinates);
+        //}
+
+        var locations = EntityQueryEnumerator<FloorTileComponent>();//smth else
+        while (locations.MoveNext(out _, out _))
+        {
+            if (!TryComp<TransformComponent>(ent, out var transform))
+                continue;
+
+            validLocations.Add(transform.Coordinates);
+        }
+
+        var teleportLocation = _random.Pick(validLocations);
+
+        var xform = Transform(ent);
+        _transformSystem.SetCoordinates(ent, xform, teleportLocation);
     }
 }
