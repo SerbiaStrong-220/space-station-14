f52f0e24ceb2884bdc4dd63b964777f483f6d9cf
SkaldetSkaeg <impotekh@gmail.com>
2025-11-18 01:05:50 +0700

    tweaks

--- Content.Server/SS220/StationEvents/Components/AdditionalRulesComponent.cs
+++ Content.Server/SS220/StationEvents/Components/AdditionalRulesComponent.cs
@@ -1,6 +1,6 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
-using Robust.Shared.Prototypes;
+using Content.Shared.EntityTable.EntitySelectors;
 
 namespace Content.Server.SS220.StationEvents.Components;
 
@@ -8,8 +8,8 @@ namespace Content.Server.SS220.StationEvents.Components;
 /// Component when it is necessary to add a gamerule with a chance during initialization
 /// </summary>
 [RegisterComponent]
-public sealed partial class AdditionalRuleChanceComponent : Component
+public sealed partial class AdditionalRulesComponent : Component
 {
     [DataField(required: true)]
-    public Dictionary<EntProtoId, float> Rules = [];
+    public Dictionary<string, EntityTableSelector> Rules = [];
 }
--- Content.Server/SS220/StationEvents/Events/AdditionalRules.cs
+++ Content.Server/SS220/StationEvents/Events/AdditionalRules.cs
@@ -4,35 +4,36 @@ using Content.Server.Administration.Logs;
 using Content.Server.GameTicking;
 using Content.Server.SS220.StationEvents.Components;
 using Content.Shared.Database;
+using Content.Shared.EntityTable;
 using Robust.Shared.Random;
 
 namespace Content.Server.SS220.StationEvents.Events;
 
-public sealed class AdditionalRuleChanceSystem : EntitySystem
+public sealed class AdditionalRulesSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly GameTicker _ticker = default!;
     [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly EntityTableSystem _entityTable = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<AdditionalRuleChanceComponent, ComponentInit>(OnInit);
+        SubscribeLocalEvent<AdditionalRulesComponent, ComponentInit>(OnInit);
     }
 
-    private void OnInit(Entity<AdditionalRuleChanceComponent> ent, ref ComponentInit args)
+    private void OnInit(Entity<AdditionalRulesComponent> ent, ref ComponentInit args)
     {
         foreach (var kvp in ent.Comp.Rules)
         {
-            if (!_random.Prob(kvp.Value))
-                continue;
+            var rule = _entityTable.GetSpawns(kvp.Value);
 
             _ticker.AddGameRule(kvp.Key);
 
             _adminLogger.Add(LogType.EventStarted,
                 $"{ToPrettyString(ent):entity} added a game rule [{kvp.Key}]" +
-                $" via a chance on AdditionalRuleChanceComponent.");
+                $" via a chance on AdditionalRulesComponent.");
         }
     }
 }
