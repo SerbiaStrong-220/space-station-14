516548c066949c1057d57601c09e88e4a20f4405
TonyBigShoes <glebasyan@yandex.ru>
2025-11-21 11:02:54 +0300

    Changed properties to fields in GrillableComponent

--- Content.Shared/SS220/Cooking/Grilling/GrillableComponent.cs
+++ Content.Shared/SS220/Cooking/Grilling/GrillableComponent.cs
@@ -13,16 +13,16 @@ namespace Content.Shared.SS220.Cooking.Grilling;
 public sealed partial class GrillableComponent : Component
 {
     [DataField]
-    public float TimeToCook { get; set; } = 120f;
+    public float TimeToCook = 120f;
 
     [DataField]
-    public string CookingResult { get; set; }
+    public string CookingResult;
 
     [ViewVariables, AutoNetworkedField]
-    public float CurrentCookTime { get; set; }
+    public float CurrentCookTime;
 
     [ViewVariables, AutoNetworkedField]
-    public bool IsCooking { get; set; }
+    public bool IsCooking;
 
     [ViewVariables]
     public SoundSpecifier CookingDoneSound = new SoundPathSpecifier("/Audio/Effects/sizzle.ogg");
