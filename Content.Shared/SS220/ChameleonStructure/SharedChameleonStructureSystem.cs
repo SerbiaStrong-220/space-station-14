deabd32ef3c491d86b15e21b54490b3b01471436
SkaldetSkaeg <impotekh@gmail.com>
2025-11-23 15:06:49 +0700

    what are you doing in my swamp

--- Content.Shared/SS220/ChameleonStructure/SharedChameleonStructureSystem.cs
+++ Content.Shared/SS220/ChameleonStructure/SharedChameleonStructureSystem.cs
@@ -1,10 +1,8 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
-using Content.Shared.Atmos.Components;
 using Content.Shared.Tag;
 using Content.Shared.Verbs;
 using Content.Shared.Whitelist;
-using Robust.Shared.Physics.Systems;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
