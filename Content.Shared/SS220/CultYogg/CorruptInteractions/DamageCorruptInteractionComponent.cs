37f9fb669b0dedf410cdeff1620c3a026d436594
SkaldetSkaeg <impotekh@gmail.com>
2025-11-25 02:16:39 +0700

    + smth

---
+++ Content.Shared/SS220/CultYogg/CorruptInteractions/DamageCorruptInteractionComponent.cs
@@ -0,0 +1,10 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
+namespace Content.Shared.SS220.CultYogg.CorruptInteractions;
+
+[RegisterComponent]
+public sealed partial class DamageCorruptInteractionComponent : Component
+{
+    [DataField(required: true), AutoNetworkedField]
+    public DamageSpecifier Damage = new();
+}
