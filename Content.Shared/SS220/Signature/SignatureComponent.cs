feff2fd7a85c2f4613fa9a9d5308515916833c4c
ReeZer2 <omfgdamndude.dude@gmail.com>
2025-11-29 18:29:55 +0200

    add photocopied feat. for signature

--- Content.Shared/SS220/Signature/SignatureComponent.cs
+++ Content.Shared/SS220/Signature/SignatureComponent.cs
@@ -1,3 +1,4 @@
+using Content.Shared.SS220.Photocopier;
 using Robust.Shared.GameStates;
 
 namespace Content.Shared.SS220.Signature;
@@ -5,8 +6,38 @@ namespace Content.Shared.SS220.Signature;
 [RegisterComponent]
 [NetworkedComponent]
 [AutoGenerateComponentState]
-public sealed partial class SignatureComponent : Component
+public sealed partial class SignatureComponent : Component, IPhotocopyableComponent
 {
     [DataField, AutoNetworkedField]
     public SignatureData? Data;
+
+    public IPhotocopiedComponentData GetPhotocopiedData()
+    {
+        return new SignaturePhotocopiedData
+        {
+            SignatureData = Data,
+        };
+    }
+}
+
+[Serializable]
+public sealed class SignaturePhotocopiedData : IPhotocopiedComponentData
+{
+    public SignatureData? SignatureData;
+
+    public bool NeedToEnsure => true;
+
+    public void RestoreFromData(EntityUid uid, Component someComponent)
+    {
+        if (someComponent is not SignatureComponent signature)
+            return;
+
+        if (SignatureData == null)
+            return;
+
+        var entMan = IoCManager.Resolve<IEntityManager>();
+
+        signature.Data = SignatureData;
+        entMan.Dirty(uid, signature);
+    }
 }
