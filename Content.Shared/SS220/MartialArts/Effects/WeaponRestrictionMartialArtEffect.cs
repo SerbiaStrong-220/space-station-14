eeed19513d9a35ccb4aab5e09a57d14d8d0bb789
Lokilife <lokilife0002@gmail.com>
2025-12-05 16:49:37 +0300

    weapon restriction now restricts throwing

--- Content.Shared/SS220/MartialArts/Effects/WeaponRestrictionMartialArtEffect.cs
+++ Content.Shared/SS220/MartialArts/Effects/WeaponRestrictionMartialArtEffect.cs
@@ -1,6 +1,7 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
 using Content.Shared.Popups;
+using Content.Shared.Throwing;
 using Content.Shared.Weapons.Melee.Events;
 using Content.Shared.Whitelist;
 
@@ -16,6 +17,7 @@ public sealed partial class WeaponRestrictionMartialArtEffectSystem : BaseMartia
         base.Initialize();
 
         SubscribeLocalEvent<MartialArtistComponent, AttemptMeleeUserEvent>(OnAttemptAttack);
+        SubscribeLocalEvent<MartialArtistComponent, ThrowAttemptEvent>(OnThrowAttempt);
     }
 
     private void OnAttemptAttack(EntityUid uid, MartialArtistComponent artist, ref AttemptMeleeUserEvent ev)
@@ -33,6 +35,23 @@ public sealed partial class WeaponRestrictionMartialArtEffectSystem : BaseMartia
 
         ev.Cancelled = true;
     }
+
+    private void OnThrowAttempt(EntityUid uid, MartialArtistComponent artist, ref ThrowAttemptEvent ev)
+    {
+        if (ev.Cancelled)
+            return;
+
+        if (!TryEffect(uid, out var effect))
+            return;
+
+        if ((effect.Whitelist == null || _whitelist.IsWhitelistPass(effect.Whitelist, ev.ItemUid)) && (effect.Blacklist == null || _whitelist.IsWhitelistFail(effect.Blacklist, ev.ItemUid)))
+            return;
+
+        // the throw attempt being raised only on server, when it will be changed by officials (i'm sure about that) you probably should change method to PopupClient - Lokilife 05.12.2025
+        _popup.PopupEntity(Loc.GetString("martial-art-effects-weapon-restriction-popup"), uid, uid);
+
+        ev.Cancel();
+    }
 }
 
 public sealed partial class WeaponRestrictionMartialArtEffect : MartialArtEffectBase<WeaponRestrictionMartialArtEffect>
@@ -42,4 +61,10 @@ public sealed partial class WeaponRestrictionMartialArtEffect : MartialArtEffect
 
     [DataField]
     public EntityWhitelist? Blacklist;
+
+    /// <summary>
+    /// When true the items that fails whitelist or blacklist cannot be thrown
+    /// </summary>
+    [DataField]
+    public bool NoThrow = true;
 }
