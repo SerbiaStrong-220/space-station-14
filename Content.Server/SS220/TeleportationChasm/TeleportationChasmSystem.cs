3900de9ab5817c916e94cffca594e6256cb2111a
SkaldetSkaeg <impotekh@gmail.com>
2025-12-10 19:14:08 +0700

    Update Content.Server/SS220/TeleportationChasm/TeleportationChasmSystem.cs
    Co-authored-by: Anri <elyasin.ane@mail.ru>

--- Content.Server/SS220/TeleportationChasm/TeleportationChasmSystem.cs
+++ Content.Server/SS220/TeleportationChasm/TeleportationChasmSystem.cs
@@ -73,8 +73,7 @@ public sealed class TeleportationChasmSystem : SharedTeleportationChasmSystem
             validLocations.Add(transform.Coordinates);
         }
 
-        if (TryTeleportFromCoordList(validLocations, ent))
-            _adminLog.Add(LogType.Teleport, $"{ent:targer} got teleported on random location on station");
+        TryTeleportFromCoordList(validLocations, ent)
     }
 
     private bool TryTeleportFromCoordList(List<EntityCoordinates> coords, EntityUid teleported)
