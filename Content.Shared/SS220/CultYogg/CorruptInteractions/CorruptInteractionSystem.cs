83d00a5ade26c4a3f4063d5a656f3b5ea581d45b
SkaldetSkaeg <impotekh@gmail.com>
2025-12-11 23:36:39 +0700

    add damage for borgs and revenant

--- Content.Shared/SS220/CultYogg/CorruptInteractions/CorruptInteractionSystem.cs
+++ Content.Shared/SS220/CultYogg/CorruptInteractions/CorruptInteractionSystem.cs
@@ -1,5 +1,8 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
+using Content.Shared.Damage;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
 using Content.Shared.SS220.CultYogg.Cultists;
 using Content.Shared.Tools.Components;
 using Content.Shared.Tools.Systems;
@@ -9,14 +12,16 @@ namespace Content.Shared.SS220.CultYogg.CorruptInteractions;
 public sealed class CorruptInteractionsSystem : EntitySystem
 {
     [Dependency] private readonly WeldableSystem _weldable = default!;
+    [Dependency] private readonly DamageableSystem _damageable = default!;
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<WeldCorruptInteractionComponent, CorruptInteractionEvent>(OnCorruptInteraction);
+        SubscribeLocalEvent<WeldCorruptInteractionComponent, CorruptInteractionEvent>(OnWeldCorruptInteraction);
+        SubscribeLocalEvent<DamageCorruptInteractionComponent, CorruptInteractionEvent>(OnDamageCorruptInteraction);
     }
 
-    private void OnCorruptInteraction(Entity<WeldCorruptInteractionComponent> ent, ref CorruptInteractionEvent args)
+    private void OnWeldCorruptInteraction(Entity<WeldCorruptInteractionComponent> ent, ref CorruptInteractionEvent args)
     {
         if (args.Handled)
             return;
@@ -25,6 +30,19 @@ public sealed class CorruptInteractionsSystem : EntitySystem
             return;
 
         _weldable.SetWeldedState(ent, !weldable.IsWelded);
+
+        args.Handled = true;
+    }
+
+    private void OnDamageCorruptInteraction(Entity<DamageCorruptInteractionComponent> ent, ref CorruptInteractionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!TryComp<DamageableComponent>(ent, out var damageableComp))
+            return;
+
+        _damageable.TryChangeDamage(ent, ent.Comp.Damage, true, interruptsDoAfters: false, damageableComp);
 
         args.Handled = true;
     }
