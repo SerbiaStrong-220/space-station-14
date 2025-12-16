f7c154b19119783fd1840c253874badf21620330
SkaldetSkaeg <impotekh@gmail.com>
2025-12-16 17:49:45 +0700

    purify cleaning

--- Content.Client/SS220/CultYogg/Cultists/PurifyingVisualizerSystem.cs
+++ Content.Client/SS220/CultYogg/Cultists/PurifyingVisualizerSystem.cs
@@ -9,6 +9,7 @@ namespace Content.Client.SS220.CultYogg.Cultists;
 /// </summary>
 public sealed class PurifyingVisualizerSystem : VisualizerSystem<CultYoggPurifiedComponent>
 {
+    [Dependency] private readonly SpriteSystem _sprite = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -21,11 +22,11 @@ public sealed class PurifyingVisualizerSystem : VisualizerSystem<CultYoggPurifie
     {
         // Need LayerMapTryGet because Init fails if there's no existing sprite / appearancecomp
         // which means in some setups (most frequently no AppearanceComp) the layer never exists.
-        if (TryComp<SpriteComponent>(uid, out var sprite) &&
-            sprite.LayerMapTryGet(PurifyingVisualLayers.Particles, out var layer))
-        {
-            sprite.RemoveLayer(layer);
-        }
+        if (!TryComp<SpriteComponent>(uid, out var sprite))
+            return;
+
+        if (_sprite.LayerMapTryGet((uid, sprite), PurifyingVisualLayers.Particles, out var layer, false))
+            _sprite.RemoveLayer((uid, sprite), layer);
     }
 
     private void OnComponentInit(Entity<CultYoggPurifiedComponent> uid, ref ComponentInit args)
@@ -33,15 +34,15 @@ public sealed class PurifyingVisualizerSystem : VisualizerSystem<CultYoggPurifie
         if (!TryComp<SpriteComponent>(uid, out var sprite) || !TryComp(uid, out AppearanceComponent? appearance))
             return;
 
-        sprite.LayerMapReserveBlank(PurifyingVisualLayers.Particles);
-        sprite.LayerSetVisible(PurifyingVisualLayers.Particles, true);
+        _sprite.LayerMapReserve((uid, sprite), PurifyingVisualLayers.Particles);
+        _sprite.LayerSetVisible((uid, sprite), PurifyingVisualLayers.Particles, true);
         sprite.LayerSetShader(PurifyingVisualLayers.Particles, "unshaded");
 
         if (uid.Comp.Sprite == null)
             return;
 
-        sprite.LayerSetRSI(PurifyingVisualLayers.Particles, uid.Comp.Sprite.RsiPath);
-        sprite.LayerSetState(PurifyingVisualLayers.Particles, uid.Comp.Sprite.RsiState);
+        _sprite.LayerSetRsi((uid, sprite), PurifyingVisualLayers.Particles, uid.Comp.Sprite.RsiPath);
+        _sprite.LayerSetRsiState((uid, sprite), PurifyingVisualLayers.Particles, uid.Comp.Sprite.RsiState);
     }
 }
 
