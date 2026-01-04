9d7e2f12a7deaa6dc743fc16177e3fffd2cdda65
ANT-14THEFIRST <anewtwinktwink@gmail.com>
2026-01-04 15:07:44 +0300

    Inhand change added

---
+++ Content.Client/SS220/ChangeAppearanceOnActiveBlockingSystem/ChangeAppearanceOnActiveBlockingSystem.cs
@@ -0,0 +1,82 @@
+using Content.Client.Items.Systems;
+using Content.Client.PDA;
+using Content.Client.SS220.FieldShield;
+using Content.Client.Toggleable;
+using Content.Shared.Blocking;
+using Content.Shared.Hands;
+using Content.Shared.Item;
+using Content.Shared.SS220.ChangeAppearanceOnActiveBLocking;
+using Content.Shared.SS220.ItemToggle;
+using Content.Shared.Toggleable;
+using Robust.Client.GameObjects;
+using System.Linq;
+
+namespace Content.Client.SS220.ChangeAppearanceOnActiveBLocking;
+public sealed partial class ChangeAppearanceOnActiveBlockingSystem : EntitySystem
+{
+    [Dependency] private readonly AppearanceSystem _appearanceSystem = default!;
+    [Dependency] private readonly SharedItemSystem _item = default!;
+    [Dependency] private readonly SpriteSystem _spriteSystem = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<ChangeAppearanceOnActiveBlockingComponent, AppearanceChangeEvent>(OnAppearanceChange);
+        SubscribeLocalEvent<ChangeAppearanceOnActiveBlockingComponent, GetInhandVisualsEvent>(OnGetHeldVisuals);
+    }
+
+    public void OnAppearanceChange(EntityUid uid, ChangeAppearanceOnActiveBlockingComponent component, ref AppearanceChangeEvent args)
+    {
+        if (!TryComp<AppearanceComponent>(uid, out var AppearanceComp))
+        {
+            return;
+        }
+        if (!TryComp<SpriteComponent>(uid, out var SpriteComp))
+        {
+            return;
+        }
+
+        if (!_appearanceSystem.TryGetData<bool>(uid, ActiveBlockingVisuals.Enabled, out var enabled, args.Component))
+            return;
+        var modulateColor =
+            _appearanceSystem.TryGetData<Color>(uid, ToggleableVisuals.Color, out var color, args.Component);
+
+        if (args.Sprite != null && component.SpriteLayer != null &&
+            _spriteSystem.LayerMapTryGet((uid, SpriteComp), component.SpriteLayer, out var layer, false))
+        {
+            _spriteSystem.LayerSetVisible((uid, SpriteComp), layer, enabled);
+            if (modulateColor)
+                _spriteSystem.LayerSetColor((uid, SpriteComp), component.SpriteLayer, color);
+        }
+        _item.VisualsChanged(uid);
+    }
+
+    private void OnGetHeldVisuals(EntityUid uid, ChangeAppearanceOnActiveBlockingComponent component, GetInhandVisualsEvent args)
+    {
+        if (!TryComp(uid, out AppearanceComponent? appearance)
+            || !_appearanceSystem.TryGetData<bool>(uid, ActiveBlockingVisuals.Enabled, out var enabled, appearance)
+            || !enabled)
+            return;
+
+        if (!component.InhandVisuals.TryGetValue(args.Location, out var layers))
+            return;
+
+        var modulateColor = _appearanceSystem.TryGetData<Color>(uid, ToggleableVisuals.Color, out var color, appearance);
+
+        var i = 0;
+        var defaultKey = $"inhand-{args.Location.ToString().ToLowerInvariant()}-toggle";
+        foreach (var layer in layers)
+        {
+            var key = layer.MapKeys?.FirstOrDefault();
+            if (key == null)
+            {
+                key = i == 0 ? defaultKey : $"{defaultKey}-{i}";
+                i++;
+            }
+
+            if (modulateColor)
+                layer.Color = color;
+
+            args.Layers.Add((key, layer));
+        }
+    }
+}
---
+++ Content.Shared/SS220/ChangeAppearanceOnActiveBlocking/ChangeAppearanceOnActiveBlockingComponent.cs
@@ -0,0 +1,32 @@
+using Content.Shared.Hands.Components;
+using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.SS220.ChangeAppearanceOnActiveBLocking;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+public sealed partial class ChangeAppearanceOnActiveBlockingComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public bool Toggled = false;
+    /// <summary>
+    /// Sprite layer that will have its visibility toggled when this item is toggled.
+    /// </summary>
+    [DataField(required: true)]
+    public string? SpriteLayer;
+
+    /// <summary>
+    /// Layers to add to the sprite of the player that is holding this entity (while the component is toggled on).
+    /// </summary>
+    [DataField]
+    public Dictionary<HandLocation, List<PrototypeLayerData>> InhandVisuals = new();
+
+    //[DataField]
+    //public ResPath StarsPath = new("Mobs/Effects/stunned.rsiObjects/Weapons/Melee/e_sword_double-inhands.rsi");
+
+    /// <summary>
+    /// Layers to add to the sprite of the player that is holding this entity (while the component is toggled on).
+    /// </summary>
+    //[DataField]
+    //public Dictionary<HandLocation, List<PrototypeLayerData>> InhandVisuals = new();
+}
