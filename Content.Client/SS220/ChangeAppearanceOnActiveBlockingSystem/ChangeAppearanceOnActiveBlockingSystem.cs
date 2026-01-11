bcc8d8249fbbdfb820d17b68269d3e268b8289cf
ANT-14THEFIRST <anewtwinktwink@gmail.com>
2026-01-11 17:15:08 +0300

    Reverting pivo reverts

--- Content.Client/SS220/ChangeAppearanceOnActiveBlockingSystem/ChangeAppearanceOnActiveBlockingSystem.cs
+++ Content.Client/SS220/ChangeAppearanceOnActiveBlockingSystem/ChangeAppearanceOnActiveBlockingSystem.cs
@@ -1,3 +1,4 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Shared.Hands;
 using Content.Shared.Item;
 using Content.Shared.SS220.ChangeAppearanceOnActiveBlocking;
