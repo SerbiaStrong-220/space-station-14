9f7869ae3a653b92f02c75b2dff91a4caf43fe70
ReeZer2 <omfgdamndude.dude@gmail.com>
2026-01-14 18:53:42 +0200

    component summary

--- Content.Shared/SS220/DragDrop/DragDropContainerTransferComponent.cs
+++ Content.Shared/SS220/DragDrop/DragDropContainerTransferComponent.cs
@@ -3,6 +3,9 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.SS220.DragDrop;
 
+/// <summary>
+/// This component marks the item that can transfer items from its storage when to be drag n dropped
+/// </summary>
 [RegisterComponent]
 [NetworkedComponent]
 public sealed partial class DragDropContainerTransferComponent : Component
