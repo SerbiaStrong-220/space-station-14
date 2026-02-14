61b870c1888cd8a1a8c98c26f27a0ce1b757c57d
Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
2026-02-14 14:33:10 +0500

    system for ammo, that can alter gun modifiers

---
+++ Content.Shared/SS220/Weapons/Ranged/Components/AmmoGunModifierComponent.cs
@@ -0,0 +1,66 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.SS220.Weapons.Ranged.Components;
+
+/// <summary>
+/// Modifies GunComponent fields values when this ammo is shot once.
+/// Applies on one and only shot of this ammo.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class AmmoGunModifierComponent : Component
+{
+    /// <summary>
+    /// Alters the sound to use when the gun is fired.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier? SoundGunshotAlt;
+
+    /// <summary>
+    /// Alters the value for the minimum angle allowed for <see cref="CurrentAngle"/>
+    /// </summary>
+    [DataField]
+    public Angle? MinAngleDelta;
+
+    /// <summary>
+    /// Alters the value for the maximum angle allowed for <see cref="CurrentAngle"/>
+    /// </summary>
+    [DataField]
+    public Angle? MaxAngleDelta;
+
+    /// <summary>
+    /// Alters the value for how much the spread increases once the gun fired.
+    /// </summary>
+    [DataField]
+    public Angle? AngleIncreaseDelta;
+
+    /// <summary>
+    /// Alters how much the <see cref="CurrentAngle"/> decreases per second.
+    /// </summary>
+    [DataField]
+    public Angle? AngleDecayDelta;
+
+    /// <summary>
+    /// Alters how many times gun shoots per second.
+    /// </summary>
+    [DataField]
+    public float? FireRateDelta;
+
+    /// <summary>
+    /// Alters how many shots to fire per burst.
+    /// </summary>
+    [DataField]
+    public int? ShotsPerBurstDelta;
+
+    /// <summary>
+    /// Alters how fast the projectile moves.
+    /// </summary>
+    [DataField]
+    public float? ProjectileSpeedDelta;
+
+    /// <summary>
+    /// Alters a scalar value applied to the vector governing camera recoil.
+    /// If reaches 0, there will be no camera recoil.
+    /// </summary>
+    [DataField]
+    public float? CameraRecoilScalarDelta;
+}
---
+++ Content.Shared/SS220/Weapons/Ranged/Systems/AmmoGunModifierSystem.cs
@@ -0,0 +1,43 @@
+using Content.Shared.Weapons.Ranged.Components;
+using Content.Shared.Weapons.Ranged.Events;
+using Content.Shared.Weapons.Ranged.Systems;
+using Content.Shared.SS220.Weapons.Ranged.Components;
+
+namespace Content.Shared.SS220.Weapons.Ranged.Systems;
+
+public sealed class AmmoGunModifierSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GunComponent, GunRefreshModifiersEvent>(OnGunRefreshModifiers);
+    }
+
+    private void OnGunRefreshModifiers(EntityUid uid, GunComponent component, ref GunRefreshModifiersEvent args)
+    {
+        if (args.CurrentAmmoModifier == null)
+            return;
+
+        var modifier = args.CurrentAmmoModifier;
+
+        if (modifier.SoundGunshotAlt != null)
+            args.SoundGunshot += modifier.SoundGunshotAlt.Value;
+        if (modifier.MinAngleDelta != null)
+            args.MinAngle += modifier.MinAngleDelta.Value;
+        if (modifier.MaxAngleDelta != null)
+            args.MaxAngle += modifier.MaxAngleDelta.Value;
+        if (modifier.AngleIncreaseDelta != null)
+            args.AngleIncrease += modifier.AngleIncreaseDelta.Value;
+        if (modifier.AngleDecayDelta != null)
+            args.AngleDecay += modifier.AngleDecayDelta.Value;
+        if (modifier.FireRateDelta != null)
+            args.FireRate += modifier.FireRateDelta.Value;
+        if (modifier.ShotsPerBurstDelta != null)
+            args.ShotsPerBurst += modifier.ShotsPerBurstDelta.Value;
+        if (modifier.ProjectileSpeedDelta != null)
+            args.ProjectileSpeed += modifier.ProjectileSpeedDelta.Value;
+        if (modifier.CameraRecoilScalarDelta != null)
+            args.CameraRecoilScalar += modifier.CameraRecoilScalarDelta.Value;
+    }
+}
