b4f7968abee8f20da186d2d7ed33a141267cb4e4
psykana <36602558+psykana@users.noreply.github.com>
2026-02-28 23:56:13 +0000

    1984

--- Content.Server/Corvax/PeacefulRoundEnd/PeacefulRoundEndSystem.cs
+++ Content.Server/Corvax/PeacefulRoundEnd/PeacefulRoundEndSystem.cs
@@ -14,9 +14,9 @@ public sealed class PeacefulRoundEndSystem : EntitySystem
     [Dependency] private readonly IConfigurationManager _cfg = default!;
     [Dependency] private readonly EmergencyShuttleSystem _emergency = default!;
     [Dependency] private readonly IPlayerManager _playerManager = default!;
-    
+
     private bool _isEnabled = false;
-    
+
     public override void Initialize()
     {
         base.Initialize();
@@ -27,26 +27,13 @@ public sealed class PeacefulRoundEndSystem : EntitySystem
     private void OnRoundEnded(RoundEndTextAppendEvent ev)
     {
         if (!_isEnabled) return;
-        // SS220-centcomm-grief-start
-        var maps = _emergency.GetCentcommMaps();
-
-        var query = AllEntityQuery<FTLMapComponent>();
-        while (query.MoveNext(out var uid, out _))
-        {
-            maps.Add(uid);
-        }
-
         foreach (var session in _playerManager.Sessions)
         {
             if (!session.AttachedEntity.HasValue) continue;
-
-            var xform = Transform(session.AttachedEntity.Value);
-            if (xform.MapUid is not null && maps.Contains(xform.MapUid.Value))
-            {
-                EnsureComp<PacifiedComponent>(session.AttachedEntity.Value);
-                EnsureComp<RoundEndPacifiedComponent>(session.AttachedEntity.Value);
-            }
+            EnsureComp<PacifiedComponent>(session.AttachedEntity.Value);
+            // SS220-centcomm-grief-start
+            EnsureComp<RoundEndPacifiedComponent>(session.AttachedEntity.Value);
+            // SS220-centcomm-grief-end
         }
-        // SS220-centcomm-grief-end
     }
 }
