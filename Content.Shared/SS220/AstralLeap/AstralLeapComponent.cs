c426c9be1609e576f1869db317154cdbe7245503
SkaldetSkaeg <impotekh@gmail.com>
2026-03-08 21:31:16 +0700

    max distance for sound + do after

--- Content.Shared/SS220/AstralLeap/AstralLeapComponent.cs
+++ Content.Shared/SS220/AstralLeap/AstralLeapComponent.cs
@@ -1,5 +1,6 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
+using Content.Shared.Polymorph;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
@@ -13,4 +14,17 @@ public sealed partial class AstralLeapComponent : Component
 
     [ViewVariables, AutoNetworkedField]
     public EntityUid? AstralActionEntity;
+
+    /// <summary>
+    ///     Preparation time for entering the astral plane
+    ///     Null if DoAfter shouldn't happen
+    /// </summary>
+    [DataField]
+    public TimeSpan? AstralLeapDoAfterTime;
+
+    /// <summary>
+    ///     An entity that will be polymorphed for the astral
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<PolymorphPrototype> AstralEnt;
 }
--- Content.Shared/SS220/AstralLeap/AstralLeapSystem.cs
+++ Content.Shared/SS220/AstralLeap/AstralLeapSystem.cs
@@ -1,12 +1,15 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
 using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Content.Shared.Polymorph;
 
 namespace Content.Shared.SS220.AstralLeap;
 
 public sealed class AstralLeapSystem : EntitySystem
 {
     [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
 
     public override void Initialize()
     {
@@ -14,6 +17,9 @@ public sealed class AstralLeapSystem : EntitySystem
 
         SubscribeLocalEvent<AstralLeapComponent, MapInitEvent>(OnInit);
         SubscribeLocalEvent<AstralLeapComponent, ComponentShutdown>(OnShutdown);
+
+        SubscribeLocalEvent<AstralLeapComponent, AstralLeapActionEvent>(OnAstralLeapAction);
+        SubscribeLocalEvent<AstralLeapComponent, AstralLeapDoAfterEvent>(OnAstralLeapDoAfter);
     }
 
     private void OnInit(Entity<AstralLeapComponent> ent, ref MapInitEvent args)
@@ -24,5 +30,36 @@ public sealed class AstralLeapSystem : EntitySystem
     private void OnShutdown(Entity<AstralLeapComponent> ent, ref ComponentShutdown args)
     {
         _actions.RemoveAction(ent.Owner, ent.Comp.AstralActionEntity);
+    }
+
+    private void OnAstralLeapAction(Entity<AstralLeapComponent> ent, ref AstralLeapActionEvent args)
+    {
+        if (ent.Comp.AstralLeapDoAfterTime is null)
+        {
+            SendToAstral(ent);
+            return;
+        }
+
+        var astralDoAfter = new DoAfterArgs(EntityManager, ent, ent.Comp.AstralLeapDoAfterTime.Value, new AstralLeapDoAfterEvent(), ent)
+        {
+            BreakOnDamage = false,
+            BreakOnMove = false,
+            BlockDuplicate = true,
+            CancelDuplicate = true,
+            DuplicateCondition = DuplicateConditions.SameEvent
+        };
+
+        _doAfter.TryStartDoAfter(astralDoAfter);
+    }
+
+    private void OnAstralLeapDoAfter(Entity<AstralLeapComponent> ent, ref AstralLeapDoAfterEvent args)
+    {
+        SendToAstral(ent);
+    }
+
+    private void SendToAstral(Entity<AstralLeapComponent> ent)
+    {
+        var ev = new PolymorphActionEvent(ent.Comp.AstralEnt);
+        RaiseLocalEvent(ent, ev);
     }
 }
