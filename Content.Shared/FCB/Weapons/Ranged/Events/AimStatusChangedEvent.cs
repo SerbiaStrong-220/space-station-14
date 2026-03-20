0f681d907e9e123a0123e42b918816f4e306ecb3
ANT-14THEFIRST <anewtwinktwink@gmail.com>
2026-03-20 15:18:00 +0300

    Moved aim attempt event to ranged events folder

--- Content.Shared/FCB/Weapons/Ranged/Events/AimStatusChangedEvent.cs
+++ Content.Shared/FCB/Weapons/Ranged/Events/AimStatusChangedEvent.cs
@@ -1,7 +1,7 @@
 // © FCB, MIT, full text: https://github.com/Free-code-base-14/space-station-14/blob/master/LICENSE.TXT
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.Weapons.Ranged.Events;
+namespace Content.Shared.FCB.Weapons.Ranged.Events;
 
 /// <summary>
 /// Raised on the client to request it change of aiming status.
