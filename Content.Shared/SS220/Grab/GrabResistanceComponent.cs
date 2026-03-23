11aeb78e0d2703a6e9f2cc5c2d74f1315ee2bda9
Lokilife <60302610+Lokilife@users.noreply.github.com>
2026-03-23 17:01:52 +0300

    balance first grab breakout attempt time

--- Content.Shared/SS220/Grab/GrabResistanceComponent.cs
+++ Content.Shared/SS220/Grab/GrabResistanceComponent.cs
@@ -21,7 +21,7 @@ public sealed partial class GrabResistanceComponent : Component
     public Dictionary<GrabStage, float> CurrentStageBreakoutChance = new();
 
     [DataField, AutoNetworkedField]
-    public TimeSpan FirstBreakoutAttemptDelay = TimeSpan.FromSeconds(10);
+    public TimeSpan FirstBreakoutAttemptDelay = TimeSpan.FromSeconds(3);
 
     [DataField, AutoNetworkedField]
     public TimeSpan BreakoutAttemptCooldown = TimeSpan.FromSeconds(30);
