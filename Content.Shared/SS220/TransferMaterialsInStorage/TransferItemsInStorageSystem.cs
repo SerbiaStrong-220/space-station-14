a334e385fee652e58790d54cf2edace4cbf82699
baynarikattu <>
2026-04-04 21:49:52 +0300

    add: transfer items from PlantBag to KitchenReagentGrinder

--- Content.Shared/SS220/TransferMaterialsInStorage/TransferItemsInStorageSystem.cs
+++ Content.Shared/SS220/TransferMaterialsInStorage/TransferItemsInStorageSystem.cs
@@ -1,13 +1,21 @@
 using System.Linq;
+using Robust.Shared.Containers;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Prototypes;
 using Content.Shared.Interaction;
 using Content.Shared.Materials;
 using Content.Shared.Storage;
+using Content.Shared.Tag;
 
 namespace Content.Shared.SS220.TransferMaterialsInStorage;
 
 public sealed class TransferMaterialsInStorageSystem : EntitySystem
 {
     [Dependency] private readonly SharedMaterialStorageSystem _material = default!;
+    [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private readonly TagSystem _tag = default!;
+
+    private static readonly ProtoId<TagPrototype> ReagentGrinderTag = "ReagentGrinder";
 
     public override void Initialize()
     {
@@ -19,20 +27,32 @@ public sealed class TransferMaterialsInStorageSystem : EntitySystem
         if (args.Handled || args.Target == null)
             return;
 
-        if (!HasComp<MaterialStorageComponent>(args.Target.Value))
-            return;
-
         if (!TryComp<StorageComponent>(ent.Owner, out var storageComponent))
             return;
 
         var items = storageComponent.Container.ContainedEntities.ToList();
 
-        foreach (var item in items)
+        if (HasComp<MaterialStorageComponent>(args.Target.Value))
         {
-            if (_material.TryInsertMaterialEntity(args.User, item, args.Target.Value))
-                continue;
+            foreach (var item in items)
+            {
+                if (_material.TryInsertMaterialEntity(args.User, item, args.Target.Value))
+                    continue;
 
-            RaiseLocalEvent(args.Target.Value, new AfterInteractUsingEvent(args.User, item, args.Target.Value, Transform(args.Target.Value).Coordinates, true));
+                RaiseLocalEvent(args.Target.Value, new AfterInteractUsingEvent(args.User, item, args.Target.Value, Transform(args.Target.Value).Coordinates, true));
+            }
+        }
+        else if (TryComp<TagComponent>(args.Target.Value, out var tag) &&
+                 _tag.HasTag(args.Target.Value, ReagentGrinderTag) &&
+                 _container.TryGetContainer(args.Target.Value, "inputContainer", out var container))
+        {
+            foreach (var item in items)
+            {
+                if (_container.Insert(item, container))
+                    continue;
+
+                RaiseLocalEvent(args.Target.Value, new AfterInteractUsingEvent(args.User, item, args.Target.Value, Transform(args.Target.Value).Coordinates, true));
+            }
         }
     }
 }
