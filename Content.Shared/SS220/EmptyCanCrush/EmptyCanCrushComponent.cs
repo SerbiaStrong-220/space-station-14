ea4f0d1a3aa40d8075e8b33acc1e45918f639c69
FilinCod <ignatkarmanov@gmail.com>
2026-04-15 18:40:17 +0300

    доделки

--- Content.Shared/SS220/EmptyCanCrush/EmptyCanCrushComponent.cs
+++ Content.Shared/SS220/EmptyCanCrush/EmptyCanCrushComponent.cs
@@ -10,7 +10,10 @@ public sealed partial class EmptyCanCrushComponent : Component
     public SoundSpecifier CrushSound =
         new SoundPathSpecifier("/Audio/SS220/Effects/can_crush.ogg")
         {
-            Params = AudioParams.Default.WithVariation(1.5f)
+            Params = AudioParams.Default
+                .WithVolume(0.2f)
+                .WithMaxDistance(5f)
+                .WithRolloffFactor(1f)
         };
 
 }
--- Content.Shared/SS220/EmptyCanCrush/EmptyCanCrushSystem.cs
+++ Content.Shared/SS220/EmptyCanCrush/EmptyCanCrushSystem.cs
@@ -1,11 +1,17 @@
 using Content.Shared.Verbs;
 using Robust.Shared.Audio.Systems;
-
+using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
+using Content.Shared.Popups;
 namespace Content.Shared.SS220.EmptyCanCrush;
 
 public sealed class EmptyCanCrushSystem : EntitySystem
 {
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly TagSystem _tagSystem = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    private static readonly ProtoId<TagPrototype> TrashTag = "Trash";
+    private static readonly ProtoId<TagPrototype> CanTag = "DrinkCan";
     public override void Initialize()
     {
         base.Initialize();
@@ -20,16 +26,25 @@ public sealed class EmptyCanCrushSystem : EntitySystem
         args.Verbs.Add(new()
         {
             Act = () => TryCrush(entity),
-            Text = Loc.GetString("can-crush-verb-text"),
-            Message = Loc.GetString("can-crush-verb-message"),
+            Text = Loc.GetString(Loc.GetString("can-crush-verb-text")),
+            Message = Loc.GetString(Loc.GetString("can-crush-verb-message")),
         });
     }
     private void TryCrush (Entity<EmptyCanCrushComponent> entity)
     {
         if (!EntityManager.TryGetComponent<TransformComponent>(entity, out var transform))
             return;
-        PredictedSpawnAtPosition(entity.Comp.CrushedCanId, transform.Coordinates);
-        _audio.PlayPredicted(entity.Comp.CrushSound, entity, entity);
-        PredictedDel(entity);
+        if (!(_tagSystem.HasTag(entity, TrashTag) && _tagSystem.HasTag(entity, CanTag)))
+        {
+            _popup.PopupPredicted(Loc.GetString("try-crush-can-false"), entity.Owner, null);
+            return;
+        }
+        var crushedCan = PredictedSpawnAtPosition(entity.Comp.CrushedCanId, transform.Coordinates);
+        if (crushedCan == null)
+            return;
+        _audio.PlayPredicted(entity.Comp.CrushSound, crushedCan, entity);
+        PredictedQueueDel(entity.Owner);
+
     }
+
 }
