934761e1b0a933c4f051ac0ccf9c292a20db24ac
FilinCod <ignatkarmanov@gmail.com>
2026-04-15 20:33:15 +0300

    доделки переделки

--- Content.Shared/SS220/EmptyCanCrush/EmptyCanCrushSystem.cs
+++ Content.Shared/SS220/EmptyCanCrush/EmptyCanCrushSystem.cs
@@ -3,6 +3,8 @@ using Robust.Shared.Audio.Systems;
 using Content.Shared.Tag;
 using Robust.Shared.Prototypes;
 using Content.Shared.Popups;
+using Content.Shared.Hands.EntitySystems;
+
 namespace Content.Shared.SS220.EmptyCanCrush;
 
 public sealed class EmptyCanCrushSystem : EntitySystem
@@ -10,6 +12,7 @@ public sealed class EmptyCanCrushSystem : EntitySystem
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly TagSystem _tagSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
     private static readonly ProtoId<TagPrototype> TrashTag = "Trash";
     private static readonly ProtoId<TagPrototype> CanTag = "DrinkCan";
     public override void Initialize()
@@ -23,26 +26,38 @@ public sealed class EmptyCanCrushSystem : EntitySystem
     {
         if (!args.CanAccess || !args.CanInteract || args.Hands == null)
             return;
+
+        var user = args.User;
         args.Verbs.Add(new()
         {
-            Act = () => TryCrush(entity),
+            Act = () => TryCrush(entity, user),
             Text = Loc.GetString(Loc.GetString("can-crush-verb-text")),
             Message = Loc.GetString(Loc.GetString("can-crush-verb-message")),
         });
     }
-    private void TryCrush (Entity<EmptyCanCrushComponent> entity)
+    private void TryCrush (Entity<EmptyCanCrushComponent> entity, EntityUid user)
     {
         if (!EntityManager.TryGetComponent<TransformComponent>(entity, out var transform))
             return;
+
         if (!(_tagSystem.HasTag(entity, TrashTag) && _tagSystem.HasTag(entity, CanTag)))
         {
             _popup.PopupPredicted(Loc.GetString("try-crush-can-false"), entity.Owner, null);
             return;
         }
+
         var crushedCan = PredictedSpawnAtPosition(entity.Comp.CrushedCanId, transform.Coordinates);
+
         if (crushedCan == null)
             return;
+
         _audio.PlayPredicted(entity.Comp.CrushSound, crushedCan, entity);
+
+        if (_handsSystem.IsHolding(user, entity, out var handID))
+        {
+            _handsSystem.TryForcePickup(user, crushedCan, handID);
+        }
+        
         PredictedQueueDel(entity.Owner);
 
     }
