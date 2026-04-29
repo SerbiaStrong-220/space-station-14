7b50b46876c5b814de92ca306d7d4db89890e4b2
Anri <elyasin.ane@mail.ru>
2026-04-29 19:28:04 +0300

    skill-issue

--- Content.Shared/Weapons/Hitscan/Events/HitscanEvents.SS220.cs
+++ Content.Shared/Weapons/Hitscan/Events/HitscanEvents.SS220.cs
@@ -1,13 +1,4 @@
-using System.Numerics;
-using Content.Shared.Damage;
-using Robust.Shared.Map;
-
 namespace Content.Shared.Weapons.Hitscan.Events;
 
-
 [ByRefEvent]
-public struct AttemptHitscanRaycastHitEvent
-{
-    public EntityUid HitScanEntity;
-    public bool Cancelled;
-}
+public record struct AttemptHitscanRaycastHitEvent(EntityUid HitScanEntity, bool Cancelled = false);
