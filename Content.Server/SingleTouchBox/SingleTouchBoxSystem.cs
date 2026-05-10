4c8a781312e7af54b1b380629a7b21c99d82a334
Dmitriy B <72795383+DmitriyBol@users.noreply.github.com>
2026-05-11 01:09:17 +0400

    review changes

--- Content.Server/SingleTouchBox/SingleTouchBoxSystem.cs
+++ Content.Server/SingleTouchBox/SingleTouchBoxSystem.cs
@@ -10,6 +10,10 @@ namespace Content.Server.SingleTouchBox;
 
 public sealed class SingleTouchBoxSystem : EntitySystem
 {
+    private const string PopupFirstLoc = "single-touch-box-popup-first";
+    private const string PopupRepeatLoc = "single-touch-box-popup-repeat";
+    private const string PopupDevourLoc = "single-touch-box-popup-devour";
+
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
     [Dependency] private readonly PopupSystem _popup = default!;
@@ -19,9 +23,7 @@ public sealed class SingleTouchBoxSystem : EntitySystem
         SubscribeLocalEvent<SingleTouchBoxComponent, UseInHandEvent>(OnUseInHand);
     }
 
-    private void OnUseInHand(
-        Entity<SingleTouchBoxComponent> ent,
-        ref UseInHandEvent args)
+    private void OnUseInHand(Entity<SingleTouchBoxComponent> ent, ref UseInHandEvent args)
     {
         var user = args.User;
         var comp = ent.Comp;
@@ -29,24 +31,15 @@ public sealed class SingleTouchBoxSystem : EntitySystem
         UpdateCombo(comp, user);
 
         if (TryDevour(ent, user))
-        {
             return;
-        }
 
         if (TryFirstUse(ent, user))
-        {
             return;
-        }
 
-        _popup.PopupCursor(
-            Loc.GetString("single-touch-box-popup-repeat"),
-            user,
-            PopupType.Small);
+        _popup.PopupCursor(Loc.GetString(PopupRepeatLoc), user, PopupType.Small);
     }
 
-    private void UpdateCombo(
-        SingleTouchBoxComponent component,
-        EntityUid user)
+    private void UpdateCombo(SingleTouchBoxComponent component, EntityUid user)
     {
         var netUser = GetNetEntity(user);
 
@@ -59,38 +52,29 @@ public sealed class SingleTouchBoxSystem : EntitySystem
         component.InteractionCount++;
     }
 
-    private bool TryFirstUse(
-        Entity<SingleTouchBoxComponent> ent,
-        EntityUid user)
+    private bool TryFirstUse(Entity<SingleTouchBoxComponent> ent, EntityUid user)
     {
         if (!ent.Comp.UsedBy.Add(user))
             return false;
 
         _popup.PopupEntity(
-            Loc.GetString("single-touch-box-popup-first"),
+            Loc.GetString(PopupFirstLoc),
             ent.Owner,
             PopupType.Medium);
 
         return true;
     }
 
-    private bool TryDevour(
-        Entity<SingleTouchBoxComponent> ent,
-        EntityUid user)
+    private bool TryDevour(Entity<SingleTouchBoxComponent> ent, EntityUid user)
     {
         if (ent.Comp.InteractionCount < ent.Comp.MaxInteractionCount)
             return false;
 
         _hands.TryDrop(user, ent.Owner);
 
-    _popup.PopupEntity(
-        Loc.GetString("single-touch-box-popup-devour"),
-        ent.Owner,
-        PopupType.LargeCaution);
+    _popup.PopupEntity(Loc.GetString(PopupDevourLoc), ent.Owner, PopupType.LargeCaution);
 
-        _audio.PlayPvs(
-            ent.Comp.DevourSound,
-            ent.Owner);
+        _audio.PlayPvs(ent.Comp.DevourSound, ent.Owner);
 
         QueueDel(user);
 
