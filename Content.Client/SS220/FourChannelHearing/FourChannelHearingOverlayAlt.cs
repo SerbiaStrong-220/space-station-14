7d155ee79d6d844b68c130209069468bdc6a3562
Kirus59 <kirus0059@gmail.com>
2026-05-26 00:29:13 +0300

    draw box tweaks

--- Content.Client/SS220/FourChannelHearing/FourChannelHearingOverlayAlt.cs
+++ Content.Client/SS220/FourChannelHearing/FourChannelHearingOverlayAlt.cs
@@ -16,28 +16,31 @@ public sealed class FourChannelHearingOverlayAlt : Overlay
 
     private readonly TransformSystem _transform;
 
-    private static readonly ProtoId<ShaderPrototype> NoiseShaderProtoId = "FourChannelHearingNoise";
-    private readonly ShaderInstance _noiseShader = default!;
+    private static readonly ProtoId<ShaderPrototype> ShaderProtoId = "FourChannelHearing";
+    private readonly ShaderInstance _shader = default!;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
 
-    private Dictionary<EntityUid, ShaderInstance> _existShaders = new();
+    private readonly Dictionary<EntityUid, ShaderInstance> _existShaders = [];
 
     public FourChannelHearingOverlayAlt()
     {
         IoCManager.InjectDependencies(this);
 
         _transform = _entity.System<TransformSystem>();
-        _noiseShader = _prototype.Index(NoiseShaderProtoId).InstanceUnique();
+        _shader = _prototype.Index(ShaderProtoId).InstanceUnique();
     }
 
-    const float _waveThikness = 0.7f;
-    const float _waveInterval = 0.2f;
+    private const float _waveThikness = 0.7f;
+    private const float _waveInterval = 0.2f;
+    private const float _waveSpeed = 1.3f;
 
-    const float _circleWaveRadius = 2.2f;
-    const float _circleWaveDecreaseStart = 1.2f;
+    private const float _circleWaveRadius = 2.2f;
+    private const float _circleWaveDecreaseStart = 1.2f;
 
-    const float _noDirectedWaveRange = 7f;
+    private const float _noDirectedWaveRange = 5f;
+
+    private static readonly Angle _directedWaveAngle = Angle.FromDegrees(15);
 
     protected override void Draw(in OverlayDrawArgs args)
     {
@@ -48,22 +51,22 @@ public sealed class FourChannelHearingOverlayAlt : Overlay
         if (!_entity.HasComponent<FourChannelHearingComponent>(player.Value))
             return;
 
-        var playerMap = _transform.GetMap(player.Value);
         var playerPos = _transform.GetWorldPosition(player.Value);
 
         var handle = args.WorldHandle;
-        var query = _entity.EntityQueryEnumerator<FourChannelHearingTargetComponent>();
-
         var zoom = args.Viewport.Eye?.Zoom ?? Vector2.One;
         var renderScale = args.Viewport.RenderScale;
         var worldToLocalMatrix = args.Viewport.GetWorldToLocalMatrix();
+
+        var query = _entity.EntityQueryEnumerator<FourChannelHearingTargetComponent>();
         while (query.MoveNext(out var uid, out var target))
         {
-            if (playerMap != _transform.GetMap(uid))
+            if (args.MapId != _transform.GetMapId(uid))
                 continue;
 
-            if (!_existShaders.TryGetValue(uid, out var shd)){
-                shd = _noiseShader.Duplicate();
+            if (!_existShaders.TryGetValue(uid, out var shd))
+            {
+                shd = _shader.Duplicate();
                 _existShaders.Add(uid, shd);
             }
 
@@ -73,27 +76,16 @@ public sealed class FourChannelHearingOverlayAlt : Overlay
             shd.SetParameter("PlayerPos", WorldToLocalPos(playerPos, args.Viewport, worldToLocalMatrix));
             shd.SetParameter("WaveThikness", WorldToLocalLength(_waveThikness, renderScale.X, zoom.X));
             shd.SetParameter("WaveInterval", WorldToLocalLength(_waveInterval, renderScale.X, zoom.X));
+            shd.SetParameter("WaveSpeed", _waveSpeed);
             shd.SetParameter("CircleWaveRadius", WorldToLocalLength(_circleWaveRadius, renderScale.X, zoom.X));
             shd.SetParameter("CircleWaveDecreaseStart", WorldToLocalLength(_circleWaveDecreaseStart, renderScale.X, zoom.X));
             shd.SetParameter("NoDirectedWaveRange", WorldToLocalLength(_noDirectedWaveRange, renderScale.X));
+            shd.SetParameter("DrawDirectedWave", !args.WorldBounds.Contains(targetPos));
+            shd.SetParameter("DirectedWaveAngle", (float)_directedWaveAngle.Theta);
             handle.UseShader(shd);
 
-            var toTargetDir = targetPos - playerPos;
-            var angle = toTargetDir.ToAngle();
             var color = target.Color.WithAlpha(0.075f);
-
-            handle.SetTransform(playerPos, angle);
-            DrawRect();
-
-            void DrawRect()
-            {
-                const float hight = -4f;
-                var p1 = new Vector2(0f, -hight);
-                var p2 = new Vector2(toTargetDir.Length() + 2.5f, hight);
-                var box = Box2.FromTwoPoints(p1, p2);
-
-                handle.DrawRect(box, color);
-            }
+            handle.DrawRect(args.WorldBounds, color);
         }
 
         handle.UseShader(null);
