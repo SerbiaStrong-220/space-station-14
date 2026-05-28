a721ecb2513fd31f1165d2eb79af25d28eb3b4c7
SkaldetSkaeg <impotekh@gmail.com>
2026-05-28 22:16:25 +0700

    finilazing

--- Content.Client/SS220/CultYogg/CultYoggIcon/ShowCultYoggIconSystem.cs
+++ Content.Client/SS220/CultYogg/CultYoggIcon/ShowCultYoggIconSystem.cs
@@ -1,11 +1,12 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
-using Content.Shared.StatusIcon;
-using Content.Shared.StatusIcon.Components;
-using Robust.Shared.Prototypes;
 using Content.Shared.SS220.CultYogg.CultYoggIcons;
 using Content.Shared.SS220.CultYogg.Sacrificials;
+using Content.Shared.SS220.CultYogg.Unenslavable;
+using Content.Shared.StatusIcon;
+using Content.Shared.StatusIcon.Components;
 using Robust.Client.Player;
+using Robust.Shared.Prototypes;
 
 namespace Content.Client.SS220.CultYogg.CultYoggIcon;
 
@@ -20,6 +21,7 @@ public sealed class ShowCultYoggIconsSystem : EntitySystem
 
         SubscribeLocalEvent<ShowCultYoggIconsComponent, GetStatusIconsEvent>(OnGetCultistsIconsEvent);
         SubscribeLocalEvent<CultYoggSacrificialComponent, GetStatusIconsEvent>(OnGetSacrificialIconsEvent);
+        SubscribeLocalEvent<UnenslavableComponent, GetStatusIconsEvent>(OnGetUnenslavableIconsEvent);
     }
 
     private void OnGetCultistsIconsEvent(Entity<ShowCultYoggIconsComponent> uid, ref GetStatusIconsEvent ev)
@@ -35,16 +37,27 @@ public sealed class ShowCultYoggIconsSystem : EntitySystem
             Log.Error($"Invalid faction icon prototype: {iconPrototype}");
     }
 
-    private void OnGetSacrificialIconsEvent(Entity<CultYoggSacrificialComponent> uid, ref GetStatusIconsEvent ev)
+    private void OnGetSacrificialIconsEvent(Entity<CultYoggSacrificialComponent> ent, ref GetStatusIconsEvent ev)
     {
         var viewer = _playerManager.LocalSession?.AttachedEntity;
-        if (viewer == uid)
+        if (viewer == ent)
             return;
 
-        if (!TryComp<CultYoggSacrificialComponent>(uid, out var sacrComp))
+        var iconId = ent.Comp.StatusIcon;
+
+        if (_prototype.TryIndex<FactionIconPrototype>(iconId, out var iconPrototype))
+            ev.StatusIcons.Add(iconPrototype);
+        else
+            Log.Error($"Invalid faction icon prototype: {iconPrototype}");
+    }
+
+    private void OnGetUnenslavableIconsEvent(Entity<UnenslavableComponent> ent, ref GetStatusIconsEvent ev)
+    {
+        var viewer = _playerManager.LocalSession?.AttachedEntity;
+        if (viewer == ent)
             return;
 
-        var iconId = sacrComp.StatusIcon;
+        var iconId = ent.Comp.StatusIcon;
 
         if (_prototype.TryIndex<FactionIconPrototype>(iconId, out var iconPrototype))
             ev.StatusIcons.Add(iconPrototype);
---
+++ Resources/Locale/ru-RU/ss220/cultYogg/examine.ftl
@@ -0,0 +1,1 @@
+cult-yogg-unenslavable-mindshied = [color=green]Цель защищена майндщилдом, её нельзя усыпить или поработить.[/color]
