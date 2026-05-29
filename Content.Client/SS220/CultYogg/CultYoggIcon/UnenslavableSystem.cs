c7b6129b958b7609de1d188a2784356b005c5fd4
SkaldetSkaeg <impotekh@gmail.com>
2026-05-29 20:41:01 +0700

    ok

--- Content.Client/SS220/CultYogg/CultYoggIcon/UnenslavableSystem.cs
+++ Content.Client/SS220/CultYogg/CultYoggIcon/UnenslavableSystem.cs
@@ -1,13 +1,14 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
-using Content.Shared.SS220.CultYogg.CultYoggIcons;
+using Content.Shared.SS220.CultYogg.Unenslavable;
+using Content.Shared.StatusIcon;
 using Content.Shared.StatusIcon.Components;
 using Robust.Client.Player;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client.SS220.CultYogg.CultYoggIcon;
 
-public sealed class ShowCultYoggIconsSystem : EntitySystem
+public sealed class UnenslavableSystem : EntitySystem
 {
     [Dependency] private readonly IPrototypeManager _prototype = default!;
     [Dependency] private readonly IPlayerManager _playerManager = default!;
@@ -16,17 +17,18 @@ public sealed class ShowCultYoggIconsSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<ShowCultYoggIconsComponent, GetStatusIconsEvent>(OnGetCultistsIconsEvent);
+        SubscribeLocalEvent<UnenslavableComponent, GetStatusIconsEvent>(OnGetUnenslavableIconsEvent);
     }
 
-    private void OnGetCultistsIconsEvent(Entity<ShowCultYoggIconsComponent> uid, ref GetStatusIconsEvent ev)
+    private void OnGetUnenslavableIconsEvent(Entity<UnenslavableComponent> ent, ref GetStatusIconsEvent ev)
     {
-        if (!TryComp<ShowCultYoggIconsComponent>(uid, out var cultComp))
+        var viewer = _playerManager.LocalSession?.AttachedEntity;
+        if (viewer == ent)
             return;
 
-        var iconId = cultComp.StatusIcon;
+        var iconId = ent.Comp.StatusIcon;
 
-        if (_prototype.TryIndex(iconId, out var iconPrototype))
+        if (_prototype.TryIndex<FactionIconPrototype>(iconId, out var iconPrototype))
             ev.StatusIcons.Add(iconPrototype);
         else
             Log.Error($"Invalid faction icon id: {iconId}");
