d9794b3487f3c8e8664b55ebd8872674837f1497
Kirus59 <kirus0059@gmail.com>
2026-05-31 17:56:52 +0300

    fix regex

--- Content.Server/SS220/Speech/EntitySystems/ResomiAccentSystem.cs
+++ Content.Server/SS220/Speech/EntitySystems/ResomiAccentSystem.cs
@@ -7,55 +7,68 @@ using Content.Shared.Speech;
 
 namespace Content.Server.SS220.Speech.EntitySystems;
 
-public sealed class ResomiAccentSystem : EntitySystem
+public sealed partial class ResomiAccentSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
 
     // Double all z/Z
-    private static readonly Regex RegexLowerZ = new("z+");
-    private static readonly Regex RegexUpperZ = new("Z+");
-    private static readonly Regex RegexRuLowerZ = new("з+");
-    private static readonly Regex RegexRuUpperZ = new("З+");
+    [GeneratedRegex("z+")] private static partial Regex LowerZRegex();
+    private static readonly string[] LowerZReplacements = ["zz", "zzz"];
+    [GeneratedRegex("Z+")] private static partial Regex UpperZRegex();
+    private static readonly string[] UpperZReplacements = ["ZZ", "ZZZ"];
+    [GeneratedRegex("з+")] private static partial Regex RuLowerZRegex();
+    private static readonly string[] RuLowerZReplacements = ["зз", "ззз"];
+    [GeneratedRegex("З+")] private static partial Regex RuUpperZRegex();
+    private static readonly string[] RuUpperZReplacements = ["ЗЗ", "ЗЗЗ"];
 
     // Insert 'sh' after 'ch'
-    private static readonly Regex RegexLowerCH = new("ch");
-    private static readonly Regex RegexUpperCH = new("CH");
-    private static readonly Regex RegexRuLowerCH = new("ч");
-    private static readonly Regex RegexRuUpperCH = new("Ч");
+    [GeneratedRegex("ch")] private static partial Regex LowerCHRegex();
+    private const string LowerCHReplacement = "chsh";
+    [GeneratedRegex("CH")] private static partial Regex UpperCHRegex();
+    private const string UpperCHReplacement = "CHSH";
+    [GeneratedRegex("ч")] private static partial Regex RuLowerCHRegex();
+    private const string RuLowerCHReplacement = "чщ";
+    [GeneratedRegex("Ч")] private static partial Regex RuUpperCHRegex();
+    private const string RuUpperCHReplacement = "ЧЩ";
 
     // Insert 'sh' after 'c'
-    private static readonly Regex RegexLowerC = new("c");
-    private static readonly Regex RegexUpperC = new("C");
-    private static readonly Regex RegexRuLowerC = new("ц");
-    private static readonly Regex RegexRuUpperC = new("Ц");
+    [GeneratedRegex("c")] private static partial Regex LowerCRegex();
+    private const string LowerCReplacement = "csh";
+    [GeneratedRegex("C")] private static partial Regex UpperCRegex();
+    private const string UpperCReplacement = "CSH";
+    [GeneratedRegex("ц")] private static partial Regex RuLowerCRegex();
+    private const string RuLowerCReplacement = "цщ";
+    [GeneratedRegex("Ц")] private static partial Regex RuUpperCRegex();
+    private const string RuUpperCReplacement = "ЦЩ";
 
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<ResomiAccentComponent, AccentGetEvent>(OnAccent);
+
+        SubscribeLocalEvent<ResomiAccentComponent, AccentGetEvent>(OnGetAccent);
     }
 
-    private void OnAccent(Entity<ResomiAccentComponent> entity, ref AccentGetEvent args)
+    private void OnGetAccent(Entity<ResomiAccentComponent> entity, ref AccentGetEvent args)
     {
         var message = args.Message;
 
         // Double all 'z'/'Z'
-        message = RegexLowerZ.Replace(message, _random.Pick(new List<string>() { "zz", "zzz" }));
-        message = RegexUpperZ.Replace(message, _random.Pick(new List<string>() { "ZZ", "ZZZ" }));
-        message = RegexRuLowerZ.Replace(message, _random.Pick(new List<string>() { "зз", "ззз" }));
-        message = RegexRuUpperZ.Replace(message, _random.Pick(new List<string>() { "ЗЗ", "ЗЗЗ" }));
+        message = LowerZRegex().Replace(message, _ => _random.Pick(LowerZReplacements));
+        message = UpperZRegex().Replace(message, _ => _random.Pick(UpperZReplacements));
+        message = RuLowerZRegex().Replace(message, _ => _random.Pick(RuLowerZReplacements));
+        message = RuUpperZRegex().Replace(message, _ => _random.Pick(RuUpperZReplacements));
 
         // "ch" into "chsh"
-        message = RegexLowerCH.Replace(message, "chsh");
-        message = RegexUpperCH.Replace(message, "CHSH");
-        message = RegexRuLowerCH.Replace(message, "чщ");
-        message = RegexRuUpperCH.Replace(message, "ЧЩ");
+        message = LowerCHRegex().Replace(message, LowerCHReplacement);
+        message = UpperCHRegex().Replace(message, UpperCHReplacement);
+        message = RuLowerCHRegex().Replace(message, RuLowerCHReplacement);
+        message = RuUpperCHRegex().Replace(message, RuUpperCHReplacement);
 
         // "c" into "csh"
-        message = RegexLowerC.Replace(message, "csh");
-        message = RegexUpperC.Replace(message, "CSH");
-        message = RegexRuLowerC.Replace(message, "цщ");
-        message = RegexRuUpperC.Replace(message, "ЦЩ");
+        message = LowerCRegex().Replace(message, LowerCReplacement);
+        message = UpperCRegex().Replace(message, UpperCReplacement);
+        message = RuLowerCRegex().Replace(message, RuLowerCReplacement);
+        message = RuUpperCRegex().Replace(message, RuUpperCReplacement);
 
         args.Message = message;
     }
