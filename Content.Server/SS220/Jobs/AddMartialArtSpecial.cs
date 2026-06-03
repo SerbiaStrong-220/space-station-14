b366986dc22495ff943df2f5177d5f6e5f4815cb
ANT-14THEFIRST <anewtwinktwink@gmail.com>
2026-06-03 17:29:37 +0300

    more markings

--- Content.Server/SS220/Jobs/AddMartialArtSpecial.cs
+++ Content.Server/SS220/Jobs/AddMartialArtSpecial.cs
@@ -1,4 +1,4 @@
-using Content.Shared.Implants;
+// © SS220, MIT full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/MIT_LICENSE.TXT
 using Content.Shared.Roles;
 using Content.Shared.SS220.MartialArts;
 using JetBrains.Annotations;
