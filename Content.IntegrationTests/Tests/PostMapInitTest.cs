3d73ee49109a92a9c830afae52344497d30d1f43
Abiergo <abiergo@inbox.ru>
2026-08-20 07:15:43 +0800

    Update PostMapInitTest.cs

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -78,7 +78,7 @@ namespace Content.IntegrationTests.Tests
             {"/Maps/Shuttles/ShuttleEvent/syndie_evacpod.yml", ["RubberStampSyndicate"]},
             {"/Maps/Shuttles/ShuttleEvent/cruiser.yml", ["ShuttleGunPerforator"]},
             {"/Maps/Shuttles/ShuttleEvent/instigator.yml", ["ShuttleGunFriendship"]},
-            {"/Maps/SS220/stargate.yml", ["CentCommFlippo"]},
+            {"/Maps/SS220/stargate.yml", ["CentCommFlippo, ShuttleGunPerforator"]},
         };
 
         /// <summary>
