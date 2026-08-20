f52bf0af33b272102a882c531df0ca4619116a17
Abiergo <abiergo@inbox.ru>
2026-08-21 03:01:39 +0800

    Revert "Update RoundEndSummaryUIController.cs"
    This reverts commit 132595a3b7967e1b7b9d52dc5b3da49c1f5ca160.

--- Content.Client/SS220/RoundEnd/RoundEndSummaryUIController.cs
+++ Content.Client/SS220/RoundEnd/RoundEndSummaryUIController.cs
@@ -100,6 +100,7 @@ public sealed partial class RoundEndSummaryUIController : UIController,
         var sponsors = message.Sponsors;
 
 #if DEBUG
+        if (IS_DEBUG_MODE)
         {
             players = players.Concat(Faker.FakeArray(50, () => new RoundEndMessageEvent.RoundEndPlayerInfo
             {
